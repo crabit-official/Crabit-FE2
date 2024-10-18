55a7b980176859470927072cae7f8c9f2d3b90d6
김용민 <dydals3440@gmail.com>
2024-10-18 16:56:07 +0900

    [CRAB-11] 내 학원, 학원 알림 페이지 구현 (#25)
    * feat: Container 컴포넌트 확장
    
    * feat: menu 구현
    
    * feat: MyAcademyAlert Page Styling
    
    * fix: 필터 모바일 환경 Z-Index 수정

--- src/app/academy/(academy)/my/page.tsx
+++ src/app/academy/(academy)/my/page.tsx
@@ -1,11 +1,18 @@
-import RowCard from '@/features/academy/my/components/row-card';
+import { HiDotsHorizontal } from 'react-icons/hi';
+
+import ListRow from '@/features/academy/alert/components/ListRow';
 import Container from '@/features/main/components/Container';
 
 function MyAcademyPage() {
   return (
     <Container>
-      <div className="flex justify-center">
-        <RowCard />
+      <div className="mt-5 h-dvh max-w-2xl rounded-md md:ml-20">
+        <ListRow
+          icon={HiDotsHorizontal}
+          contents={<ListRow.Texts title="Craft Your Habit 학원." subTitle="Crabit 학원 " />}
+          // eslint-disable-next-line @next/next/no-img-element
+          left={<img src="/images/logo_app.png" alt="이미지" />}
+        />
       </div>
     </Container>
   );
