e4fda72bcb9759292372b35a34ceca57ce1e227f
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-10-18 10:36:33 +0900

    [CRAB-9] checkbox 컴포넌트 생성 및 auth 수정 (#23)
    * feat: 회원가입 form checkbox 추가
    
    * feat: 로그인 유효성검사 추가
    
    * feat: navbar에 프로필 추가 및 logout

--- src/shared/libs/next-auth/auth-provider.tsx
+++ src/shared/libs/next-auth/auth-provider.tsx
@@ -15,7 +15,7 @@ export default function AuthProvider({ children }: IProps) {
     /* Refresh Token 만료되었을 때 */
     if (session?.error === 'RefreshAccessTokenError') {
       // alert('리프레시 토큰 만료. 로그아웃 합니다');
-      signOut({ callbackUrl: '/login' });
+      signOut({ callbackUrl: '/' });
     }
   }, [session]);
 
