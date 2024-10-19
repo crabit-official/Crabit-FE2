67fba193c1e7b0d38cdb19975ba6e634ffe57686
김용민 <dydals3440@gmail.com>
2024-10-19 09:44:09 +0900

    [Deploy] 2024년 10월 19일 Daily Deploy (#27)
    * [CRAB-3] Header 컴포넌트 구현 / 모달 컴포넌트 구현
    
    [CRAB-3] Header 컴포넌트 구현 / 모달 컴포넌트 구현
    
    * [CRAB-6] My Academy Page Folder Structure (#11)
    
    [CRAB-6] My Academy Page Folder Structure
    
    * [CRAB-6] main 페이지 ui 구현
    
    * [CRAB-7] 로그인 nextauth 연결  (#16)
    
    * feat: nextauth 로그인
    
    * feat: shared 폴더 이동
    
    * feat: 회원가입 필드 추가
    
    * feat: 회원가입 fetch로 변경
    
    * [CRAB-8] 기관 등록하기 페이지 구현 (#17)
    
    * feat: Shared Spacing Component 제작
    
    * feat: 기관 등록하기 페이지 구현 (다음 API)
    
    * fix: Spacing Component 너비 높이 공식 변경 수정
    
    ---------
    
    Co-authored-by: hyewon <89793335+hijjoy@users.noreply.github.com>
    
    * [CRAB-10] 학원 워크스페이스 폴더 구조 및 레이아웃 설정 (#20)
    
    * fix: Layout Navbar 스타일링 수정
    
    * feat: workspace folder structure
    
    * feat: workspace layout 설정
    
    * [CRAB-6] My Academy Page Folder Structure (#11)
    
    [CRAB-6] My Academy Page Folder Structure
    
    * [CRAB-10] 학원 워크스페이스 폴더 구조 및 레이아웃 설정 (#20)
    
    * fix: Layout Navbar 스타일링 수정
    
    * feat: workspace folder structure
    
    * feat: workspace layout 설정
    
    * [CRAB-6] My Academy Page Folder Structure (#11)
    
    [CRAB-6] My Academy Page Folder Structure
    
    * [CRAB-10] 학원 워크스페이스 폴더 구조 및 레이아웃 설정 (#20)
    
    * fix: Layout Navbar 스타일링 수정
    
    * feat: workspace folder structure
    
    * feat: workspace layout 설정
    
    * [CRAB-9] checkbox 컴포넌트 생성 및 auth 수정 (#23)
    
    * feat: 회원가입 form checkbox 추가
    
    * feat: 로그인 유효성검사 추가
    
    * feat: navbar에 프로필 추가 및 logout
    
    * [CRAB-11] 내 학원, 학원 알림 페이지 구현 (#25)
    
    * feat: Container 컴포넌트 확장
    
    * feat: menu 구현
    
    * feat: MyAcademyAlert Page Styling
    
    * fix: 필터 모바일 환경 Z-Index 수정
    
    * [CRAB-10] dashboard page (#26)
    
    * feat: Intercepting Routes
    
    * feat: flex start 추가
    
    * feat: dashboard layout
    
    * feat: 파일이름 변경
    
    * feat: unsplash이미지 추가
    
    * feat: 가로 스크롤 추가
    
    ---------
    
    Co-authored-by: hyewon <89793335+hijjoy@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,11 @@ const nextConfig = {
                 // server일시 browser를 제외 시킨다
                 config.resolve.alias.push({ name: "msw/browser", alias: false });
             else config.resolve.alias["msw/browser"] = false;
-        } else if (Array.isArray(config.resolve.alias))
+        } else {
+            if (Array.isArray(config.resolve.alias))
                 config.resolve.alias.push({ name: "msw/node", alias: false });
             else config.resolve.alias["msw/node"] = false;
+        }
         return config;
     },
     logging: {
@@ -30,7 +32,7 @@ const nextConfig = {
                 port: "",
                 pathname: "/**",
             },
-            
+
         ],
     },
 };
---
+++ src/app/academy/(workspace)/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import WorkspaceTab from '@/features/academy/(workspace)/components/workspace-tab';
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <WorkspaceTab />
+      <div className="xl:px-30 max-w-8xl container mx-auto h-full">
+        <div className="grid">
+          <div className="col-span-4 border-x lg:col-span-3">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
