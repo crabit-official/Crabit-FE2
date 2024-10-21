e083cc2f363b72e6bf82571c0c4af6468a6111d6
hijjoy <hijjoy@naver.com>
2024-10-21 14:37:25 +0900

    feat: 학원 권한 에러처리 수정

--- src/app/academy/(workspace)/[id]/layout.tsx
+++ src/app/academy/(workspace)/[id]/layout.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { getServerSession } from 'next-auth';
 
+import Error from '@/features/academy/(workspace)/components/error';
 import WorkspaceTab from '@/features/academy/(workspace)/components/workspace-tab';
 import type { IAcademyResponse } from '@/shared/types/acadmy';
 import { authOptions } from '@/shared/utils/authOptions';
 
 async function Layout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
-  const session = await getServerSession(authOptions);
-  const accessToken = session?.accessToken;
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${params.id}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${accessToken}`,
-    },
-  });
-
-  const data: IAcademyResponse = await res.json();
-  const role = data?.result?.academyRole;
+  const role = await getAcademyResponse(params.id);
 
   if (!role) {
-    // TODO: 에러 메세지 수정
-    return <div>학원에 대한 권한이 없습니다.</div>;
+    return <Error label="학원에 대한 접근 권한이 없습니다." className="pt-20" />;
   }
 
   return (
     <>
-      <WorkspaceTab role={role} />
+      <WorkspaceTab role={role} />;
       <div className="xl:px-30 max-w-8xl container mx-auto h-full">
         <div className="grid">
           <div className="col-span-4 border-x lg:col-span-3">{children}</div>
@@ -35,6 +23,24 @@ async function Layout({ children, params }: { children: React.ReactNode; params:
       </div>
     </>
   );
+}
+
+async function getAcademyResponse(id: string) {
+  const session = await getServerSession(authOptions);
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${id}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
+
+  const data: IAcademyResponse = await res.json();
+  const role = data?.result?.academyRole;
+
+  if (!role) return null;
+
+  return role;
 }
 
 export default Layout;
