b7fec699eddcc035b511c1ab6ca82a1c9c1bd72e
hijjoy <hijjoy@naver.com>
2024-10-21 15:14:06 +0900

    feat: 챌린지 추가 버튼

--- src/app/academy/(workspace)/[id]/layout.tsx
+++ src/app/academy/(workspace)/[id]/layout.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { getServerSession } from 'next-auth';
 
+import getAcademyProfile from '@/features/academy/(workspace)/api/get-academy-profile';
 import Error from '@/features/academy/(workspace)/components/error';
 import WorkspaceTab from '@/features/academy/(workspace)/components/workspace-tab';
-import type { IAcademyResponse } from '@/shared/types/acadmy';
-import { authOptions } from '@/shared/utils/authOptions';
 
 async function Layout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
-  const role = await getAcademyResponse(params.id);
+  const role = await getAcademyProfile(params.id);
 
   if (!role) {
     return <Error label="학원에 대한 접근 권한이 없습니다." className="pt-20" />;
@@ -15,7 +13,7 @@ async function Layout({ children, params }: { children: React.ReactNode; params:
 
   return (
     <>
-      <WorkspaceTab role={role} />;
+      <WorkspaceTab role={role} />
       <div className="xl:px-30 max-w-8xl container mx-auto h-full">
         <div className="grid">
           <div className="col-span-4 border-x lg:col-span-3">{children}</div>
@@ -23,24 +21,6 @@ async function Layout({ children, params }: { children: React.ReactNode; params:
       </div>
     </>
   );
-}
-
-async function getAcademyResponse(id: string) {
-  const session = await getServerSession(authOptions);
-
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${id}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${session?.accessToken}`,
-    },
-  });
-
-  const data: IAcademyResponse = await res.json();
-  const role = data?.result?.academyRole;
-
-  if (!role) return null;
-
-  return role;
 }
 
 export default Layout;
--- src/features/academy/(workspace)/components/dashboard/Sidebar/index.tsx
+++ src/features/academy/(workspace)/components/dashboard/Sidebar/index.tsx
@@ -1,17 +1,27 @@
 'use client';
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { FaCirclePlus } from 'react-icons/fa6';
 
 import ChallengeMenu from '@/features/academy/(workspace)/components/dashboard/ChallengeMenu';
 import Profile from '@/features/academy/(workspace)/components/dashboard/Profile';
+import MenuItem from '@/features/main/components/MenuItem';
 import Flex from '@/shared/components/Flex';
 import { useDraggable } from '@/shared/hooks/useDraggable';
 
-function Sidebar() {
+interface ISidebarProps {
+  role: string;
+}
+
+function Sidebar({ role }: ISidebarProps) {
   const { containerRef, handleMouseDown, handleMouseMove, handleMouseUpOrLeave } = useDraggable<HTMLUListElement>();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = useCallback(() => {
+    setIsOpen((open) => !open);
+  }, []);
 
   return (
-    <Flex column="center" className="items-center gap-14 md:flex md:items-start md:justify-start">
+    <Flex row="center" className="items-center gap-3 md:flex md:flex-col md:items-start md:justify-start md:gap-14">
       <Flex className="hidden max-w-52 md:flex">
         <Profile />
       </Flex>
@@ -29,6 +39,23 @@ function Sidebar() {
         <ChallengeMenu label="코딩 모닝" />
         <ChallengeMenu label="미라클 모닝" />
       </ul>
+      {role !== 'STUDENT' && (
+        <button type="button" onClick={toggleOpen} className="relative w-fit md:w-full">
+          <FaCirclePlus size={20} className="text-slate-500 hover:text-black" />
+          {isOpen && (
+            <div className="absolute right-0 z-10 w-[150px] max-w-[170px] overflow-hidden rounded-xl bg-white text-sm shadow-md md:left-0 md:top-10 md:w-full">
+              <div className="flex cursor-pointer flex-col">
+                <MenuItem
+                  onClick={() => {
+                    setIsOpen(false);
+                  }}
+                  label="챌린저 추가하기"
+                />
+              </div>
+            </div>
+          )}
+        </button>
+      )}
     </Flex>
   );
 }
