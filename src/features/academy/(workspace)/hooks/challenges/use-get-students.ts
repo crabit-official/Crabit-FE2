d74dcee9f3c5fee33c0db28ca489d469bf611428
hijjoy <hijjoy@naver.com>
2024-10-22 19:41:06 +0900

    feat: 무한 스크롤 type

--- src/features/academy/(workspace)/hooks/challenges/use-get-students.ts
+++ src/features/academy/(workspace)/hooks/challenges/use-get-students.ts
@@ -1,27 +1,69 @@
+import type { DefaultError, InfiniteData, QueryKey, UseInfiniteQueryOptions } from '@tanstack/react-query';
 import { useInfiniteQuery } from '@tanstack/react-query';
 
-import type { IAcademyResponse, IAllStudents } from '@/shared/types/acadmy';
-
-const getAllStudents = async ({ id, cursor, accessToken }: { accessToken: string; cursor: number; id: string }) => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${id}/students?cursor=${cursor}&take=10`, {
+const getAllStudents = async ({ id, cursor, accessToken, take }: { accessToken: string; cursor: number; id: string; take: number }) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${id}/students?cursor=${cursor}&take=${take}`, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
   });
 
-  const data: IAcademyResponse<IAllStudents> = await res.json();
-  return data;
+  if (!res.ok) {
+    throw new Error('Failed to fetch students');
+  }
+
+  const data: AcademyStudentListResponse = await res.json();
+
+  return data; // Assuming result is the part that conforms to AcademyStudentListResponse
 };
 
-const useGetStudents = ({ id, accessToken }: { accessToken: string; id: string }) =>
-  useInfiniteQuery({
-    queryKey: ['students', id],
-    queryFn: ({ pageParam = 0 }) => getAllStudents({ id, cursor: pageParam, accessToken }),
-    getNextPageParam: (lastPage) => lastPage.nextCursor || null,
-    initialPageParam: 0,
-    enabled: !!accessToken && !!id,
-    select: (data) => data.pages,
+type TStudentList = {
+  introduction: string;
+  nickname: string;
+  profileImageUrl: string;
+  school: string;
+  studentId: number;
+};
+
+export type AcademyStudentListResponse = {
+  result: {
+    hasNext: boolean;
+    nextCursor: number;
+    studentList: TStudentList[];
+  };
+};
+
+function useGetStudents(
+  id: string,
+  accessToken: string,
+  take: number,
+  queryOptions?: UseInfiniteQueryOptions<
+    AcademyStudentListResponse,
+    DefaultError,
+    InfiniteData<AcademyStudentListResponse, number>,
+    AcademyStudentListResponse,
+    QueryKey,
+    number
+  >,
+) {
+  return useInfiniteQuery({
+    queryFn: ({ pageParam }) => getAllStudents({ id, cursor: pageParam, accessToken, take }),
+    queryKey: ['academyStudents', id],
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => (lastPage.result.hasNext ? allPages.length + 1 : undefined),
+    ...queryOptions,
   });
+}
+
+// const useGetStudents = ({ id, accessToken }: { accessToken: string; id: string }) =>
+//   useInfiniteQuery({
+//     queryKey: ['students', id],
+//     queryFn: ({ pageParam = 0 }) => getAllStudents({ id, cursor: pageParam, accessToken }),
+//     getNextPageParam: (lastPage) => lastPage.nextCursor || null,
+//     initialPageParam: 0,
+//     enabled: !!accessToken && !!id,
+//     select: (data) => data.pages,
+//   });
 
 export default useGetStudents;
