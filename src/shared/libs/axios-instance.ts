8df082419ea11196b8ee21b4e017158dc551dbd8
김용민 <dydals3440@gmail.com>
2024-10-26 14:29:23 +0900

    [CRAB-23] (원장) 학생 리스트 조회, 학생 리스트 화면 구현 (#50)

--- src/shared/libs/axios-instance.ts
+++ src/shared/libs/axios-instance.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosConfig = {
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
-    'Content-Type': 'application/json',
+    'Content-Type': 'members/json',
   },
 };
 
