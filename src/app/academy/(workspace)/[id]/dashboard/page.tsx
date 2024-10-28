d0f19c46371aa311f37d5ec48d3e2e13560ab781
hijjoy <hijjoy@naver.com>
2024-10-29 03:25:01 +0900

    feat: dashboard 학생 연결

--- src/app/academy/(workspace)/[id]/dashboard/page.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { dehydrate, QueryClient } from '@tanstack/query-core';
+import { HydrationBoundary } from '@tanstack/react-query';
 import { getServerSession, type Session } from 'next-auth';
 
+import AllChallengeContents from '@/app/academy/(workspace)/[id]/dashboard/components/AllChallengeContents';
 import getAcademyProfile from '@/features/academy/(workspace)/api/get-academy-profile';
 import BestChallengeTable from '@/features/academy/(workspace)/components/dashboard/best-challenge-table';
 import ChartView from '@/features/academy/(workspace)/components/dashboard/chart-view';
 import { getTop5Students } from '@/shared/apis/academy';
+import { getAllChallengeContents } from '@/shared/apis/challenge';
+import { queryKeys } from '@/shared/constants/query-keys';
 import { ACADEMY_ROLE } from '@/shared/enums/academy';
 import { authOptions } from '@/shared/utils/authOptions';
 
@@ -13,11 +18,24 @@ async function AcademyDashBoardPage({ params }: { params: { id: string } }) {
   const data = await getAcademyProfile(Number(params.id));
   const topStudents = await getTop5Students({ session, academyId: Number(params.id) });
 
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [queryKeys.ALL_CHALLENGE_LIST],
+    queryFn: () => getAllChallengeContents({ session, cursor: 0, take: 5, academyId: Number(params.id) }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) => (lastPage.result.hasNext ? allPages.length + 1 : undefined),
+    pages: 1,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   if (data?.academyRole === ACADEMY_ROLE.STUDENT) {
     return (
-      <div className="grid gap-40">
-        <BestChallengeTable topStudents={topStudents} />
-      </div>
+      <HydrationBoundary state={dehydratedState}>
+        <div className="grid gap-40">
+          <BestChallengeTable topStudents={topStudents} />
+          <AllChallengeContents session={session} academyId={Number(params.id)} />
+        </div>
+      </HydrationBoundary>
     );
   }
 
