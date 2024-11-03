ec6b9a7ca3aad81e898c6f6fd8721cf803e47e87
hijjoy <hijjoy@naver.com>
2024-11-03 15:59:47 +0900

    feat: navber size & nsc 이미지 추가

--- src/shared/components/Logo/index.tsx
+++ src/shared/components/Logo/index.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 function Logo() {
   const router = useRouter();
 
-  return <Image onClick={() => router.push('/')} className="cursor-pointer" src="/images/logo_light.png" alt="Crabit_Logo" height="100" width="100" />;
+  return <Image onClick={() => router.push('/')} className="cursor-pointer" src="/images/logo_light.png" alt="Crabit_Logo" height="80" width="80" />;
 }
 
 export default Logo;
