6defc8229e589336093ff7a56ebc52b09f79454a
hijjoy <hijjoy@naver.com>
2024-11-08 17:19:31 +0900

    feat: 챌린지 마켓 tab 변경

--- src/app/academy/(workspace)/[id]/market/components/ChallengeTab/index.tsx
+++ src/app/academy/(workspace)/[id]/market/components/ChallengeTab/index.tsx
@@ -1,31 +1,37 @@
 'use client';
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import React from 'react';
+import { motion } from 'framer-motion';
+import { useRouter, useSearchParams } from 'next/navigation';
 
-function ChallengeTab() {
-  const currentPath = usePathname();
+const menu = [
+  { tab: 'crabit', text: '크래빗 공식' },
+  { tab: 'academy', text: '학원 챌린지' },
+];
 
+interface IChallengeTabProps {
+  academyId: number;
+}
+
+function ChallengeTab({ academyId }: IChallengeTabProps) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const tab = searchParams.get('tab') || 'crabit';
+
+  const handleTabChange = (tabName: string) => {
+    router.push(`/academy/${academyId}/market/?tab=${tabName}`);
+  };
   return (
-    <ul className="flex gap-4">
-      <li
-        className={`${currentPath === '/academy/123/market' ? 'border-b-2 border-solid border-main-pink font-bold text-main-deep-pink' : 'text-gray-600'} h-7 transition-transform duration-300 hover:scale-95 hover:border-b-2 hover:border-solid hover:border-main-pink hover:font-bold hover:text-main-deep-pink`}
-      >
-        <Link href="market" className="text-lg">
-          크래빗 공식
-        </Link>
-      </li>
-      <li className="text-gray-600' h-7 cursor-not-allowed">
-        <div className="text-lg">학원 챌린지</div>
-      </li>
-      {/* <li */}
-      {/*  className={`${currentPath === '/academy/123/academy' ? 'border-b-2 border-solid border-main-pink font-bold text-main-deep-pink' : 'text-gray-600'} h-7 transition-transform duration-300 hover:scale-95 hover:border-b-2 hover:border-solid hover:border-main-pink hover:font-bold hover:text-main-deep-pink`} */}
-      {/* > */}
-      {/*  <Link href="market/academy" className="text-lg"> */}
-      {/*    학원 챌린지 */}
-      {/*  </Link> */}
-      {/* </li> */}
-    </ul>
+    <motion.div className="flex w-full justify-center">
+      <ul className="flex gap-3">
+        {menu.map((item, idx) => (
+          <li key={idx} onClick={() => handleTabChange(item.tab)} className="relative cursor-pointer py-1">
+            <p className={`${tab === item.tab ? 'font-bold text-main-deep-pink' : 'font-medium text-gray-600'}`}>{item.text}</p>
+            {tab === item.tab && <motion.div layoutId="underline" className="absolute inset-x-0 bottom-0 h-[2px] bg-main-deep-pink" />}
+          </li>
+        ))}
+      </ul>
+    </motion.div>
   );
 }
 
