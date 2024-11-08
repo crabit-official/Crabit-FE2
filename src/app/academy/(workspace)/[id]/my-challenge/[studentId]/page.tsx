ed2b339ea1607bdf43440f265fc2261376fc447c
hijjoy <hijjoy@naver.com>
2024-11-08 21:51:51 +0900

    chore: 사용하지 않는 파일 삭제 및 수정

--- src/app/academy/(workspace)/[id]/my-challenge/[studentId]/page.tsx
+++ src/app/academy/(workspace)/[id]/my-challenge/[studentId]/page.tsx
@@ -1,20 +1,17 @@
 import { Suspense } from 'react';
 import { dehydrate, QueryClient } from '@tanstack/query-core';
 import { HydrationBoundary } from '@tanstack/react-query';
-import { getServerSession, type Session } from 'next-auth';
 
 import MyChallengeList from '@/features/academy/(workspace)/components/my-challenge-list';
 import { getStudentChallengeList } from '@/shared/apis/challenge';
 import Flex from '@/shared/components/Flex';
 import { queryKeys } from '@/shared/constants/query-keys';
-import { authOptions } from '@/shared/utils/authOptions';
 
 async function MyChallengePage({ params }: { params: { id: string } }) {
-  const session = (await getServerSession(authOptions)) as Session;
   const queryClient = new QueryClient();
   await queryClient.prefetchInfiniteQuery({
     queryKey: [queryKeys.CHALLENGE_LIST],
-    queryFn: () => getStudentChallengeList({ session, cursor: 0, take: 5, academyId: Number(params.id) }),
+    queryFn: () => getStudentChallengeList({ cursor: 0, take: 5, academyId: Number(params.id) }),
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) => (lastPage.result.hasNext ? allPages.length + 1 : undefined),
     pages: 1,
@@ -25,7 +22,7 @@ async function MyChallengePage({ params }: { params: { id: string } }) {
     <HydrationBoundary state={dehydratedState}>
       <Flex rowColumn="center" className="w-full gap-4 px-4 pt-14">
         <Suspense fallback={<div>Loading</div>}>
-          <MyChallengeList academyId={Number(params.id)} session={session} />
+          <MyChallengeList academyId={Number(params.id)} />
         </Suspense>
       </Flex>
     </HydrationBoundary>
