657b27d133f8bdaf6317fd181b3565fb2a2030ca
hijjoy <hijjoy@naver.com>
2024-11-08 14:34:29 +0900

    feat: 학원 접근 에러처리

--- src/app/academy/(workspace)/[id]/layout.tsx
+++ src/app/academy/(workspace)/[id]/layout.tsx
@@ -14,9 +14,13 @@ interface ILayoutProps {
 async function Layout({ children, params }: ILayoutProps) {
   const res = await fetchData<IAcademyResponse<IAcademyProfile>>(`/api/v1/academies/${Number(params.id)}`, 'GET');
 
+  if (!res?.result?.academyRole) {
+    return <div>학원에 접근할 수 없습니다.</div>;
+  }
+
   return (
     <>
-      <WorkspaceTab role={res.result.academyRole} memberId={res.result.memberId} />
+      <WorkspaceTab role={res?.result.academyRole} memberId={res?.result.memberId} />
       <div className="xl:px-30 max-w-8xl container mx-auto h-full">
         <div className="grid">
           <div className="col-span-4 place-items-center border-x lg:col-span-3">{children}</div>
