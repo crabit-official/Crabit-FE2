b832657f6feabf36eb5c7fc1f9cdc38f24a3973d
김용민 <dydals3440@gmail.com>
2024-11-15 15:58:24 +0900

    [CRAB-43] 학원 가입 신청 모달 패널 구현 (#103)
    * feat: 학원 초대코드 가입 화면 구현
    
    * feat: Invitation Modal 제작
    
    * fix: Register Modal ...register을 잃는 현상 해결
    
    * feat: 학원 가입 신청 구현

---
+++ src/app/academy/(academy)/my/components/invitation-modal/components/Tab.tsx
@@ -0,0 +1,46 @@
+import type { PropsWithChildren } from 'react';
+
+import cn from '@/shared/utils/style';
+
+interface ITabs {
+  activeTab: number;
+  setActiveTab: (tab: number) => void;
+}
+
+function Tabs({ children }: PropsWithChildren) {
+  return <div className="flex flex-col">{children}</div>;
+}
+
+function TabList({ children }: PropsWithChildren) {
+  return <div className="flex justify-center gap-x-20">{children}</div>;
+}
+
+function Tab({ children, index, activeTab, setActiveTab }: PropsWithChildren<{ index: number } & ITabs>) {
+  return (
+    <button
+      type="button"
+      className={cn('border-b-3 px-14 py-5', {
+        'font-bold text-main-deep-pink': activeTab === index,
+        'border-transparent font-bold text-gray-500': activeTab !== index,
+      })}
+      onClick={() => setActiveTab(index)}
+    >
+      {children}
+    </button>
+  );
+}
+
+function TabPanels({ children }: PropsWithChildren) {
+  return <div className="flex-1">{children}</div>;
+}
+
+function TabPanel({ children, index, activeTab }: PropsWithChildren<{ activeTab: number; index: number }>) {
+  return <div hidden={index !== activeTab}>{children}</div>;
+}
+
+Tabs.TabList = TabList;
+Tabs.Tab = Tab;
+Tabs.TabPanels = TabPanels;
+Tabs.TabPanel = TabPanel;
+
+export default Tabs;
