a402ec8f552b2a4ecfd0cf403d85c4236c5d86de
hijjoy <hijjoy@naver.com>
2024-11-15 17:49:05 +0900

    feat: Menubar 수정

--- src/app/academy/(workspace)/[id]/dashboard/components/MenuItem/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/MenuItem/index.tsx
@@ -3,24 +3,42 @@
 import React, { useState } from 'react';
 import { GoX } from 'react-icons/go';
 import { AnimatePresence, motion } from 'framer-motion';
+import Link from 'next/link';
 
-function MenuItem({ text }: { text: string }) {
+import type { ITabMenu } from '@/shared/constants/tab-menu';
+
+interface IMenuItemProps {
+  academyId: number;
+  activeTab: string;
+  content: ITabMenu[];
+  title: string;
+  type?: 'public-challenge' | 'dashboard';
+}
+
+function MenuItem({ title, content, type = 'dashboard', academyId, activeTab }: IMenuItemProps) {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleOpen = () => setIsOpen(!isOpen);
+  const selectedTab = activeTab || 'all';
+
   return (
     <motion.ul className="border-b border-solid border-gray-300" layout onClick={toggleOpen}>
       <motion.div layout className="flex cursor-pointer items-center justify-between py-4 text-lg font-bold">
-        <p>{text}</p>
+        <p>{title}</p>
         <GoX className={`${isOpen ? 'rotate-180' : 'rotate-45'} transition-transform duration-500`} />
       </motion.div>
       <ul>
         <AnimatePresence>
           {isOpen && (
-            <motion.div className="pb-3" layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-              <p className="cursor-pointer pb-3 pl-5 text-gray-700">우리 학원 공개</p>
-              <p className="cursor-pointer pb-3 pl-5 text-gray-700">크래빗 공개</p>
-              <p className="cursor-pointer pb-3 pl-5 text-gray-700">비공개 챌린지</p>
+            <motion.div className="flex flex-col pb-3" layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+              {content.map((tab) => (
+                <Link
+                  href={`/academy/${academyId}/${type}?tab=${tab.tab}`}
+                  key={tab.tab}
+                  className={`${selectedTab === tab.tab ? 'text-main-deep-pink' : 'text-gray-700'} cursor-pointer pb-3 pl-5 hover:text-main-deep-pink`}
+                >
+                  {tab.text}
+                </Link>
+              ))}
             </motion.div>
           )}
         </AnimatePresence>
