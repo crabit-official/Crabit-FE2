105e5c8ba4aee9ca59dfde0ce6249f7b6686e0ee
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-11-18 18:59:07 +0900

    [CRAB-48] 학생 챌린지 feed, my 챌린지 로딩 수정 (#113)
    * feat: 학생 tab 수정
    - 챌린지 피드 추가
    
    * feat: 대시보드 학생 챌린지 tab 수정
    - feed 삭제 (전체 tab으로 이동)
    
    * feat: MY 챌린지 로딩 스캘레톤 수정
    
    * feat: 챌린지 피드 api 연결
    
    * feat: 챌린지 content ui 수정
    
    * feat: 챌린지 content 이미지 처리
    - default 이미지 삭제

--- src/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeFeed/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeFeed/index.tsx
@@ -9,15 +9,14 @@ import StudentChallengeContent from '@/app/academy/(workspace)/[id]/dashboard/co
 import Flex from '@/shared/components/Flex';
 import Framer from '@/shared/components/Framer';
 import Typography from '@/shared/components/Typography';
-import useInfiniteMyChallengeContents from '@/shared/hooks/challenge/useInfiniteMyChallengeContents';
+import useGetInfiniteFeedChallengeContents from '@/shared/hooks/challenge/useGetInfiniteFeedChallengeContents';
 
-interface IMyChallengeProps {
+interface IFeedProps {
   academyId: number;
-  studentChallengeId: number;
 }
 
-function MyChallenge({ academyId, studentChallengeId }: IMyChallengeProps) {
-  const { data: contents, isError, isFetching, hasNextPage, fetchNextPage } = useInfiniteMyChallengeContents(academyId, studentChallengeId);
+function ChallengeFeed({ academyId }: IFeedProps) {
+  const { data: contents, isFetching, hasNextPage, fetchNextPage, isError } = useGetInfiniteFeedChallengeContents(academyId);
   const isEmpty = contents?.pages.every((page) => page.result.challengeLogList.length === 0);
 
   const { ref, inView } = useInView({
@@ -47,14 +46,14 @@ function MyChallenge({ academyId, studentChallengeId }: IMyChallengeProps) {
         <Image src="/images/icons/icon_bell.webp" alt="bell img" width={200} height={200} />
         <Flex column="start" className="w-80 rounded-2xl border border-solid border-gray-200 bg-gray-100 px-5 py-4">
           <Typography size="h4" as="p" className="opacity-80">
-            챌린지 글이 없습니다
+            인증글을 작성해주세요 !
           </Typography>
-          <Typography size="h7" as="p" className="font-normal opacity-60">
-            오늘의 인증글을 작성해주세요.
+          <Typography size="h7" as="p" className="whitespace-pre-wrap font-normal opacity-60">
+            {`챌린지를 시작한 상태인 챌린지에 대한\n인증 게시글만 조회 가능합니다.`}
           </Typography>
           <div className="mt-7">
-            <Link href="?tab=create" className="rounded-md bg-main-deep-pink px-4 py-2 text-sm font-semibold text-white hover:opacity-60">
-              작성하기
+            <Link href={`/academy/${academyId}/dashboard`} className="rounded-md bg-main-deep-pink px-4 py-2 text-sm font-semibold text-white hover:opacity-60">
+              작성하러 가기
             </Link>
           </div>
         </Flex>
@@ -64,7 +63,6 @@ function MyChallenge({ academyId, studentChallengeId }: IMyChallengeProps) {
 
   return (
     <Flex column="center" className="w-full gap-4">
-      <div>디자인 수정 예정입니다...</div>
       {contents?.pages.map((page) =>
         page.result.challengeLogList.map((content) => (
           <StudentChallengeContent
@@ -83,5 +81,4 @@ function MyChallenge({ academyId, studentChallengeId }: IMyChallengeProps) {
     </Flex>
   );
 }
-
-export default MyChallenge;
+export default ChallengeFeed;
