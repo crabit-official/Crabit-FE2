31fd69525c7d6835abe98ebaec1d74e3d5a7edc3
hijjoy <hijjoy@naver.com>
2024-11-20 16:46:15 +0900

    feat: role 별 대시보드 메뉴 구분

--- src/app/academy/(workspace)/[id]/dashboard/components/Menubar/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/Menubar/index.tsx
@@ -6,15 +6,17 @@ import { useRouter } from 'next/navigation';
 
 import MenuItem from '@/app/academy/(workspace)/[id]/dashboard/components/MenuItem';
 import Flex from '@/shared/components/Flex';
-import { PUBLIC_MENU } from '@/shared/constants/tab-menu';
+import { PUBLIC_MENU, SUBMISSION_STATU_MENU } from '@/shared/constants/tab-menu';
+import { ACADEMY_ROLE } from '@/shared/enums/academy';
 import useDebounce from '@/shared/hooks/useDebounce';
 
 interface IMenubarProps {
   academyId: number;
   activeTab: string;
+  role: ACADEMY_ROLE;
 }
 
-function Menubar({ academyId, activeTab }: IMenubarProps) {
+function Menubar({ academyId, activeTab, role }: IMenubarProps) {
   const router = useRouter();
   const [search, setSearch] = useState<string>('');
   const debounceSearch = useDebounce(search.trim(), 500);
@@ -30,7 +32,11 @@ function Menubar({ academyId, activeTab }: IMenubarProps) {
   return (
     <Flex column="start" className="gap-4">
       <ul>
-        <MenuItem title="카테고리" content={PUBLIC_MENU} academyId={academyId} activeTab={activeTab} />
+        {role === ACADEMY_ROLE.STUDENT ? (
+          <MenuItem title="챌린지 상태" content={SUBMISSION_STATU_MENU} academyId={academyId} activeTab={activeTab} />
+        ) : (
+          <MenuItem title="카테고리" content={PUBLIC_MENU} academyId={academyId} activeTab={activeTab} />
+        )}
       </ul>
       <Flex row="between" className="w-full items-center rounded-xl border border-solid border-gray-200 bg-gray-50 px-3 py-2">
         <input
