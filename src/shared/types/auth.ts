25f492969d2f8c179deb322053bfab7c78998227
hijjoy <hijjoy@naver.com>
2024-11-22 11:55:20 +0900

    feat: getProfile type 변경 - enum 추가 - response type 변경

--- src/shared/types/auth.ts
+++ src/shared/types/auth.ts
@@ -1,3 +1,5 @@
+import type { GLOBAL_ROLE, SOCIAL_TYPE } from '@/shared/enums/auth';
+
 export interface ICommonResponse {
   code: string;
   isSuccess: true;
@@ -7,10 +9,21 @@ export interface ICommonResponse {
 export interface IAuthResponse extends ICommonResponse {
   result: {
     accessToken: string;
-    globalRole: string;
+    globalRole: GLOBAL_ROLE;
     name: string;
     profileImageUrl: string;
     refreshToken: string;
-    socialType: string;
+    socialType: SOCIAL_TYPE;
+  };
+}
+
+export interface IProfileResponse extends ICommonResponse {
+  result: {
+    email: string;
+    globalRole: GLOBAL_ROLE;
+    memberId: 0;
+    name: string;
+    profileImageUrl: string;
+    socialType: SOCIAL_TYPE;
   };
 }
