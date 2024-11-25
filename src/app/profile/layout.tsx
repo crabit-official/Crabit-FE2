ae6e5a4d0f0564a52d703e2748fb1283f3335760
hijjoy <hijjoy@naver.com>
2024-11-25 16:57:12 +0900

    feat: 마이페이지 레이아웃 간격 수정

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -12,7 +12,7 @@ function Layout({ children }: ILayoutProps) {
     <Flex rowColumn="center" className="size-full px-10 sm:px-20 sm:py-10">
       <section className="grid size-full min-h-[700px] max-w-[1000px] grid-cols-1 gap-4 bg-transparent lg:grid-cols-[min-content,auto] lg:bg-second-gradient">
         <Sidebar />
-        <Flex className="h-full rounded-xl border border-solid border-gray-200 bg-white px-10 py-5">{children}</Flex>
+        <Flex className="h-full rounded-xl border border-solid border-gray-200 bg-white p-5 sm:px-10">{children}</Flex>
       </section>
     </Flex>
   );
