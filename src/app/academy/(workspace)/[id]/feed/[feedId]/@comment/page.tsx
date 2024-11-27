0b0a8729db4be9b2ef6d636362295dc82d13ba27
hijjoy <hijjoy@naver.com>
2024-11-27 10:39:52 +0900

    feat: 댓글 prefetching

--- src/app/academy/(workspace)/[id]/feed/[feedId]/@comment/page.tsx
+++ src/app/academy/(workspace)/[id]/feed/[feedId]/@comment/page.tsx
@@ -1,6 +1,11 @@
+import { dehydrate, QueryClient } from '@tanstack/query-core';
+import { HydrationBoundary } from '@tanstack/react-query';
+
 import CommentForm from '@/app/academy/(workspace)/[id]/feed/components/CommentForm';
 import CommentList from '@/app/academy/(workspace)/[id]/feed/components/CommentList';
+import { getCommentList } from '@/shared/apis/comments';
 import Flex from '@/shared/components/Flex';
+import { queryKeys } from '@/shared/constants/query-keys';
 
 interface ICommentProps {
   params: {
@@ -12,11 +17,27 @@ interface ICommentProps {
   };
 }
 
-export default function Comment({ params, searchParams }: ICommentProps) {
+export default async function Comment({ params, searchParams }: ICommentProps) {
+  const academyId = Number(params.id);
+  const releasedChallengeId = Number(params.feedId);
+  const studentChallengeLogId = Number(searchParams.log);
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [queryKeys.COMMENT_LIST, { academyId }, { releasedChallengeId }],
+    queryFn: ({ pageParam }) => getCommentList({ academyId, cursor: pageParam, take: 10, studentChallengeLogId, releasedChallengeId }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => (lastPage.result.hasNext ? lastPage.result.nextCursor : undefined),
+    pages: 1,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <Flex column="start">
-      <CommentForm academyId={Number(params.id)} releasedChallengeId={Number(params.feedId)} studentChallengeLogId={Number(searchParams.log)} />
-      <CommentList academyId={Number(params.id)} releasedChallengeId={Number(params.feedId)} studentChallengeLogId={Number(searchParams.log)} />
+      <CommentForm academyId={academyId} releasedChallengeId={releasedChallengeId} studentChallengeLogId={studentChallengeLogId} />
+      <HydrationBoundary state={dehydratedState}>
+        <CommentList academyId={academyId} releasedChallengeId={releasedChallengeId} studentChallengeLogId={studentChallengeLogId} />
+      </HydrationBoundary>
     </Flex>
   );
 }
