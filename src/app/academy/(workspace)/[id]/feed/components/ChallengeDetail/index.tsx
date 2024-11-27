0eaa36dab4253d758f9041eedba349523bd0cf07
hijjoy <hijjoy@naver.com>
2024-11-27 11:31:08 +0900

    feat: 피드 상세 이미지 크기 수정

--- src/app/academy/(workspace)/[id]/feed/components/ChallengeDetail/index.tsx
+++ src/app/academy/(workspace)/[id]/feed/components/ChallengeDetail/index.tsx
@@ -34,7 +34,7 @@ function ChallengeDetail({ studentAcademyProfile, studentChallengeLog }: TChalle
           alt="file image"
           width={300}
           height={300}
-          className="h-48 w-full rounded-lg border border-solid border-gray-100 bg-gray-50 object-contain"
+          className="size-full rounded-lg border border-solid border-gray-100 bg-gray-50 object-contain"
         />
       )}
       <Typography size="h6" className="font-medium opacity-80">
