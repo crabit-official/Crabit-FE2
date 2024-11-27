ed268201b17516a55b706ac1fc86f8d1d601f295
김용민 <dydals3440@gmail.com>
2024-11-27 14:18:46 +0900

    feat: 학생 관리

--- src/shared/apis/challenge.ts
+++ src/shared/apis/challenge.ts
@@ -81,7 +81,7 @@ export async function getStudentChallengeList({ cursor, take, academyId, categor
 
 // [원장 선생님] 학원 전체 학생 리스트 조회
 export async function getAcademyMemberDetailList({ cursor, take, academyId, nickname }: IGetAcademyMemberDetailList) {
-  let url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${academyId}/students/detail?cursor=${cursor}&take=${take}`;
+  let url = `/api/academy/manage/admin/students?academyId=${academyId}&cursor=${cursor}&take=${take}`;
 
   if (nickname) {
     url += `&nickname=${nickname}`;
@@ -92,7 +92,7 @@ export async function getAcademyMemberDetailList({ cursor, take, academyId, nick
   });
 
   if (!res.ok) {
-    throw new Error('학원의 학생 목록을 가져오는데 에러가 발생했습니다!');
+    throw new Error('학원의 선생님 목록을 가져오는데 에러가 발생했습니다!');
   }
 
   const data: IAcademyMemberListResult = await res.json();
