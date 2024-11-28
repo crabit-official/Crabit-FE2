378850b955a0e2d697e2e3c288a64dcc11927b5b
hijjoy <hijjoy@naver.com>
2024-11-28 10:57:09 +0900

    feat: 챌린지 생성 후 router 이동

--- src/shared/hooks/challenge/useCreateChallengeContent.ts
+++ src/shared/hooks/challenge/useCreateChallengeContent.ts
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 import { createChallengeContent } from '@/shared/apis/challenge';
@@ -10,12 +11,14 @@ interface ICreateChallengeContentProps {
 }
 
 function useCreateChallengeContent({ academyId, studentChallengeId }: ICreateChallengeContentProps) {
+  const router = useRouter();
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: createChallengeContent,
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: [queryKeys.MY_CHALLENGE_CONTENTS, { academyId }, { studentChallengeId }] });
       toast.success('인증 글이 생성되었습니다.');
+      router.push(`/academy/${academyId}/dashboard/${studentChallengeId}?tab=my-challenge`);
     },
     onError: (error) => {
       toast.error(error.message);
