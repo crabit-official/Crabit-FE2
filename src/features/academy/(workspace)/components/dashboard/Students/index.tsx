31b25684ac682f2c218238543bc41a045deb968e
hijjoy <hijjoy@naver.com>
2024-11-29 12:04:32 +0900

    feat: memberName -> name 으로 변경

--- src/features/academy/(workspace)/components/dashboard/Students/index.tsx
+++ src/features/academy/(workspace)/components/dashboard/Students/index.tsx
@@ -7,7 +7,7 @@ interface IStudentsProps extends IAcademyStudentListDTO {
   setSelectedStudentIdList: Dispatch<SetStateAction<number[]>>;
 }
 
-function Students({ school, memberName, academyMemberId, selectedStudentIdList, setSelectedStudentIdList }: IStudentsProps) {
+function Students({ school, name, academyMemberId, selectedStudentIdList, setSelectedStudentIdList }: IStudentsProps) {
   const isSelected = selectedStudentIdList.includes(academyMemberId);
 
   const handleSelect = () => {
@@ -23,7 +23,7 @@ function Students({ school, memberName, academyMemberId, selectedStudentIdList, 
       onClick={handleSelect}
       className={`h-fit cursor-pointer rounded-md p-2 hover:opacity-80 ${isSelected ? 'bg-main-pink font-medium text-white' : 'bg-neutral-100 text-black'}`}
     >
-      {school} | {memberName}
+      {school} | {name}
     </div>
   );
 }
