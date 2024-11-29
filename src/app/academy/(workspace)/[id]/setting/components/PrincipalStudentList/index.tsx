af8208d78bb047c19c37c166a54b1681c71217bf
hijjoy <hijjoy@naver.com>
2024-11-29 09:55:49 +0900

    feat: (원장) 학생 관리 리스트 연결

---
+++ src/app/academy/(workspace)/[id]/setting/components/PrincipalStudentList/index.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+import Flex from '@/shared/components/Flex';
+import ProfileCard from '@/shared/components/ProfileCard';
+import Typography from '@/shared/components/Typography';
+import useGetInfiniteAcademyMemberDetailList from '@/shared/hooks/academy/useGetInfiniteAcademyMemberDetailList';
+
+interface IStudentListProps {
+  academyId: number;
+}
+
+function PrincipalStudentList({ academyId }: IStudentListProps) {
+  const { data: studentList, isFetching, hasNextPage, fetchNextPage, isError } = useGetInfiniteAcademyMemberDetailList(9, academyId);
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      if (!isFetching && hasNextPage) {
+        void fetchNextPage();
+      }
+    }
+  }, [inView, isFetching, hasNextPage, fetchNextPage]);
+
+  if (isError) {
+    return (
+      <Flex>
+        <Typography size="h5">에러가 발생했습니다.</Typography>
+      </Flex>
+    );
+  }
+
+  return (
+    <div className="grid w-full grid-cols-1 gap-5 pl-0 md:pl-10 lg:grid-cols-2 xl:grid-cols-3">
+      {studentList?.pages.map((page) => page.result.studentList.map((student) => <ProfileCard {...student} key={student.academyMemberId} />))}
+      {isFetching
+        ? Array(9)
+            .fill('')
+            .map((_, i) => <ProfileCard.Skeleton key={i} />)
+        : null}
+      <div ref={ref} className="h-5" />
+    </div>
+  );
+}
+
+export default PrincipalStudentList;
--- src/app/academy/(workspace)/[id]/setting/management/student/page.tsx
+++ src/app/academy/(workspace)/[id]/setting/management/student/page.tsx
@@ -1,6 +1,14 @@
+import PrincipalStudentList from '@/app/academy/(workspace)/[id]/setting/components/PrincipalStudentList';
+import { fetchData } from '@/shared/apis/fetch-data';
 import ProfileCard from '@/shared/components/ProfileCard';
+import { ACADEMY_ROLE } from '@/shared/enums/academy';
+import type { IAcademyProfile, IAcademyResponse } from '@/shared/types/acadmy';
 
-function StudentManagementPage() {
+async function StudentManagementPage({ params }: { params: { id: string } }) {
+  const res = await fetchData<IAcademyResponse<IAcademyProfile>>(`/api/v1/academies/${Number(params.id)}`, 'GET');
+
+  if (res.result.academyRole === ACADEMY_ROLE.PRINCIPAL) return <PrincipalStudentList academyId={Number(params.id)} />;
+
   return (
     <div className="grid w-full grid-cols-1 gap-5 pl-0 md:pl-10 lg:grid-cols-2 xl:grid-cols-3">
       <ProfileCard name="안예원" nickname="면 싫어" school="대진고등학교" description="예원이는 ISTP" point={1000} />
