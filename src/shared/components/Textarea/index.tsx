82288b1cc5d7668a6ae094ec7e471f02217f3c85
hijjoy <hijjoy@naver.com>
2024-11-29 11:58:59 +0900

    feat: Textarea 수정 - h-full - gray ver. 색상추가

--- src/shared/components/Textarea/index.tsx
+++ src/shared/components/Textarea/index.tsx
@@ -1,10 +1,28 @@
 'use client';
 
+import React from 'react';
 import type { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
+import { cva, type VariantProps } from 'class-variance-authority';
 
 import cn from '@/shared/utils/style';
 
-interface IInputProps {
+const TextAreaVariants = cva(
+  'size-full peer rounded-md p-4 pt-6 outline-none transition disabled:cursor-not-allowed disabled:opacity-70 resize-none overflow-y-auto min-h-44',
+  {
+    variants: {
+      variant: {
+        main: 'border-2 bg-main-white border-neutral-300 focus:border-main-black',
+        secondary: 'bg-gray-50 border border-solid border-gray-200 focus:border-gray-300',
+      },
+    },
+
+    defaultVariants: {
+      variant: 'main',
+    },
+  },
+);
+
+interface IInputProps extends VariantProps<typeof TextAreaVariants> {
   className?: string;
   disabled?: boolean;
   errors: FieldErrors;
@@ -15,7 +33,7 @@ interface IInputProps {
   type?: string;
 }
 
-function TextArea({ id, label, disabled, required, register, errors, className }: IInputProps) {
+function TextArea({ id, label, disabled, required, register, errors, className, variant }: IInputProps) {
   return (
     <div className={cn(`relative w-full`, className)}>
       <textarea
@@ -23,7 +41,7 @@ function TextArea({ id, label, disabled, required, register, errors, className }
         disabled={disabled}
         {...register(id, { required })}
         placeholder=" "
-        className={`'pl-4' peer h-44 w-full rounded-md border-2 bg-main-white p-4 pt-6 font-light outline-none transition disabled:cursor-not-allowed disabled:opacity-70 ${errors[id] ? 'border-main-pink' : 'border-neutral-300'} ${errors[id] ? 'focus:border-main-pink' : 'focus:border-main-black'} resize-none overflow-y-auto`}
+        className={cn(TextAreaVariants({ variant }), `${errors[id] && "focus:border-main-pink'} border-main-pink"} `)}
       />
       <label
         className={`absolute left-4 top-5 z-10 origin-[0] -translate-y-3 text-h6 duration-150 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 ${errors[id] ? 'text-main-pink' : 'text-zinc-400'} `}
