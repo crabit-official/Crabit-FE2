7c356387954dc44662690cbd810a7925bb57a695
김용민 <dydals3440@gmail.com>
2024-11-29 10:00:45 +0900

    [Deploy] 2024년 11월 29일 Daily Deploy (#182)
    * [CRAB-77] Protected Route 적용 #181
    
    ---------
    
    Co-authored-by: hyewon <89793335+hijjoy@users.noreply.github.com>
    Co-authored-by: hijjoy <hijjoy@naver.com>

--- src/app/academy/(workspace)/[id]/layout.tsx
+++ src/app/academy/(workspace)/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 import WorkspaceTab from '@/features/academy/(workspace)/components/workspace-tab';
 
@@ -12,6 +13,11 @@ interface ILayoutProps {
 
 async function Layout({ children, params }: ILayoutProps) {
   const cookieStore = cookies();
+
+  const accessToken = cookies().get('accessToken')?.value;
+  if (!accessToken) {
+    redirect('/');
+  }
 
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${Number(params.id)}`, {
     method: 'GET',
