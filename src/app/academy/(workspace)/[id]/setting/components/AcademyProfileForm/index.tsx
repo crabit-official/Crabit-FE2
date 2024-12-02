3e18f8401f00b4b65a1e27bd304dd2b635f67b58
hijjoy <hijjoy@naver.com>
2024-12-02 10:29:51 +0900

    feat: 설정 ui 수정

--- src/app/academy/(workspace)/[id]/setting/components/AcademyProfileForm/index.tsx
+++ src/app/academy/(workspace)/[id]/setting/components/AcademyProfileForm/index.tsx
@@ -1,19 +1,21 @@
 'use client';
 
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from 'react';
+import { type FieldValues, useForm } from 'react-hook-form';
 import { FaPencil } from 'react-icons/fa6';
 import { PiUserSquareFill } from 'react-icons/pi';
 import { useQueryClient } from '@tanstack/react-query';
 import Image from 'next/image';
-import { useParams } from 'next/navigation';
 import { toast } from 'sonner';
-import { z } from 'zod';
 
+import StateLabel from '@/features/academy/(workspace)/components/state-label';
 import { useImage } from '@/features/academy/(workspace)/hooks/use-image';
+import Avatar from '@/shared/components/Avatar';
 import BoxContainer from '@/shared/components/BoxContainer';
 import Button from '@/shared/components/Button';
 import Flex from '@/shared/components/Flex';
+import FramerScale from '@/shared/components/FramerScale';
+import Input from '@/shared/components/Input';
 import Skeleton from '@/shared/components/Skeleton/Skeleton';
 import Spacing from '@/shared/components/Spacing/spacing';
 import Typography from '@/shared/components/Typography';
@@ -22,45 +24,39 @@ import { ACADEMY_ROLE } from '@/shared/enums/academy';
 import useManageAcademy from '@/shared/hooks/academy/useManageAcademy';
 import useGetPresignedUrl from '@/shared/hooks/images/use-get-presigned-url';
 import { getRoleName } from '@/shared/utils/academyRole';
+import { formatNumberWithCommas } from '@/shared/utils/number';
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const formSchema = z.object({
-  academyRole: z.union([z.literal(ACADEMY_ROLE.PRINCIPAL), z.literal(ACADEMY_ROLE.INSTRUCTOR), z.literal(ACADEMY_ROLE.STUDENT)]),
-  nickname: z.string(),
-  profileImageUrl: z.string().url(),
-  school: z.string(),
-  point: z.number().int(),
-  file: z.string(),
-  introduction: z.string().max(500),
-});
+interface IProfileFormProps {
+  academyId: number;
+}
 
-type FormValues = z.infer<typeof formSchema>;
-
-function AcademyProfileForm() {
+function AcademyProfileForm({ academyId }: IProfileFormProps) {
   let profileContent;
   const queryClient = useQueryClient();
-  const params = useParams();
   const [edit, setEdit] = useState<boolean>(false);
   const { filePreview, handleChangeFile, file } = useImage();
 
   const { useGetAcademyMemberProfile, editAcademyMemberProfile } = useManageAcademy();
 
-  const { data: profile, isPending: profileLoading } = useGetAcademyMemberProfile({ academyId: Number(params.id) });
+  const { data: profile, isPending: profileLoading } = useGetAcademyMemberProfile({ academyId });
   const { data: image, isSuccess } = useGetPresignedUrl(file?.name as string);
 
-  const { register, handleSubmit } = useForm<FormValues>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
     defaultValues: profile
       ? {
-          academyRole: profile?.result.academyRole,
           nickname: profile?.result.nickname,
           profileImageUrl: profile?.result.profileImageUrl,
+          introduction: profile?.result.introduction,
           school: profile?.result.school,
-          point: profile?.result.point,
         }
       : undefined,
   });
 
-  const onSubmit = async (data: FormValues) => {
+  const onSubmit = async (data: FieldValues) => {
     const editNickname = data.nickname ?? profile?.result.nickname;
     const editIntroduction = data.introduction ?? profile?.result.introduction;
     const editSchool = data.school ?? profile?.result.school;
@@ -79,14 +75,14 @@ function AcademyProfileForm() {
             editAcademyMemberProfile.mutate(
               {
                 profileImageUrl: image.result.keyName,
-                academyId: Number(params.id),
+                academyId,
                 introduction: editIntroduction,
                 nickname: editNickname,
                 school: editSchool,
               },
               {
                 onSuccess: () => {
-                  queryClient.invalidateQueries({ queryKey: [queryKeys.ACADEMY_MEMBER_PROFILE, Number(params.id)] });
+                  queryClient.invalidateQueries({ queryKey: [queryKeys.ACADEMY_MEMBER_PROFILE, { academyId }] });
                 },
               },
             );
@@ -99,14 +95,14 @@ function AcademyProfileForm() {
       editAcademyMemberProfile.mutate(
         {
           profileImageUrl: profile?.result.profileImageUrl ?? null,
-          academyId: Number(params.id),
+          academyId,
           introduction: editIntroduction,
           nickname: editNickname,
           school: editSchool,
         },
         {
           onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: [queryKeys.ACADEMY_MEMBER_PROFILE, Number(params.id)] });
+            queryClient.invalidateQueries({ queryKey: [queryKeys.ACADEMY_MEMBER_PROFILE, { academyId }] });
           },
         },
       );
@@ -120,40 +116,38 @@ function AcademyProfileForm() {
       <>
         <label
           htmlFor="file"
-          className="flex size-52 cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-solid border-gray-200 bg-gray-50"
+          className="flex size-20 cursor-pointer flex-col items-center justify-center gap-2 rounded-full border border-solid border-gray-200 bg-gray-50"
         >
           {filePreview ? (
-            <Image src={filePreview} width={200} height={200} className="size-52 rounded-xl object-cover" alt="img" />
+            <Image src={filePreview} width={200} height={200} className="size-52 rounded-full object-cover" alt="img" />
           ) : profile?.result.profileImageUrl ? (
             <Image
               src={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${profile.result.profileImageUrl}`}
-              width={200}
-              height={200}
-              className="size-52 rounded-xl object-cover"
+              width={100}
+              height={100}
+              className="size-20 rounded-full border border-solid border-gray-200 object-cover"
               alt="img"
             />
           ) : (
-            <PiUserSquareFill size={30} />
+            <PiUserSquareFill size={15} />
           )}
         </label>
-        <input type="file" id="file" {...register('file')} onChange={handleChangeFile} className="hidden" />
+        <input type="file" id="file" onChange={handleChangeFile} className="hidden" />
       </>
     );
   } else {
     profileContent = profile?.result.profileImageUrl ? (
-      <div className="flex size-52 cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-solid border-gray-200 bg-gray-50">
+      <div className="flex size-20 flex-col items-center justify-center gap-2 rounded-full border border-solid border-gray-200 bg-gray-50">
         <Image
           src={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${profile.result.profileImageUrl}`}
-          width={200}
-          height={200}
-          className="size-52 rounded-xl border border-solid border-gray-200 object-cover"
+          width={100}
+          height={100}
+          className="size-20 rounded-full border border-solid border-gray-200 object-cover"
           alt="img"
         />
       </div>
     ) : (
-      <div className="flex size-52 cursor-pointer flex-col items-center justify-center gap-2 rounded-xl border border-solid border-gray-200 bg-gray-50">
-        <PiUserSquareFill size={30} />
-      </div>
+      <Avatar size="lg" />
     );
   }
 
@@ -166,9 +160,6 @@ function AcademyProfileForm() {
           <Skeleton width={60} height={20} className="rounded-sm" />
         </Flex>
         <Spacing direction="vertical" size={20} />
-        <Flex row="end">
-          <Skeleton width={60} height={40} className="rounded-md" />
-        </Flex>
         <Spacing direction="vertical" size={20} />
         <Flex>
           <Skeleton height={330} width={500} className="w-full rounded-md" />
@@ -178,109 +169,88 @@ function AcademyProfileForm() {
   }
 
   return (
-    <Flex column="start" className="w-full gap-5">
-      <Flex column="start" className="w-full gap-1">
-        <Typography size="h3" className="opacity-80">
-          {edit ? '프로필 수정' : '내 프로필'}
-        </Typography>
-        <Typography size="h5" as="p" className="text-xs opacity-60">
-          기본 프로필
-        </Typography>
-        {edit && <Spacing direction="vertical" size={28} />}
-      </Flex>
-      <Flex row="end">
+    <FramerScale className="flex w-full flex-col justify-start gap-5">
+      <section className="relative flex flex-col gap-4">
         {!edit && (
-          <Button variant="secondary" type="button" className="w-16 font-medium text-slate-500" onClick={() => setEdit(true)}>
-            <FaPencil />
+          <Button
+            variant="secondary"
+            type="button"
+            className="absolute right-[-10px] top-[-15px] w-fit rounded-full p-[10px] hover:bg-main-deep-pink hover:text-white"
+            onClick={() => setEdit(true)}
+          >
+            <FaPencil size={15} />
           </Button>
         )}
-      </Flex>
-      <section className="flex flex-col gap-4">
-        <BoxContainer variant="border" className="flex w-[500px] flex-col gap-5 sm:flex-row">
-          <Flex as="figure" row="start">
-            {profileContent}
+        <BoxContainer className="flex w-full flex-col items-center gap-5 p-8 pb-14">
+          <Flex column="start" className="w-full gap-1">
+            <Typography size="h3" className="opacity-80">
+              {edit ? '프로필 수정' : '내 프로필'}
+            </Typography>
+            <Typography size="h5" as="p" className="text-xs opacity-60">
+              학원 내 프로필 수정
+            </Typography>
+            {edit && <Spacing direction="vertical" size={28} />}
           </Flex>
-
-          <Flex column="between" className="w-full py-2">
-            <Flex column="start">
-              <Flex className="items-center gap-2">
-                <Typography size="h5" as="p" className="w-20 text-center text-sm font-semibold opacity-80 lg:text-base">
-                  역할
-                </Typography>
-                <Typography size="h5" as="p" className="w-full p-2 text-sm font-normal opacity-80 lg:text-base">
-                  {getRoleName(profile?.result.academyRole as ACADEMY_ROLE)}
-                </Typography>
-              </Flex>
-              <Flex className="items-center gap-2">
-                <Typography size="h5" as="p" className="w-20 text-center text-sm font-semibold opacity-80 lg:text-base">
-                  닉네임
-                </Typography>
-                {edit ? (
-                  <input
-                    defaultValue={profile?.result.nickname}
-                    disabled={profileLoading}
-                    id="nickname"
-                    {...register('nickname', { required: true })}
-                    placeholder="이름을 입력해주세요."
-                    className="w-full border-b border-solid border-b-gray-200 bg-transparent p-2 text-sm focus:border-b-main-deep-pink focus:outline-none lg:text-base"
-                  />
-                ) : (
-                  <Typography size="h5" as="p" className="w-full p-2 text-sm font-normal opacity-80 lg:text-base">
-                    {profile?.result.nickname}
-                  </Typography>
-                )}
-              </Flex>
-              <Flex className="items-center gap-2">
-                <Typography size="h5" as="p" className="w-20 text-center text-sm font-semibold opacity-80 lg:text-base">
-                  소개
+          <Flex as="figure" rowColumn="center" className="relative">
+            {profileContent}
+            {profile?.result.academyRole === ACADEMY_ROLE.STUDENT && !!profile?.result.point && !edit && (
+              <StateLabel label={`Ⓟ ${formatNumberWithCommas(Number(profile?.result.point))}`} variant="yellow" className="absolute bottom-[-10px]" />
+            )}
+          </Flex>
+          <Typography size="h7" as="p" className="w-full text-center font-normal">
+            {getRoleName(profile?.result.academyRole as ACADEMY_ROLE)} {profile?.result.academyRole === ACADEMY_ROLE.STUDENT && `• ${profile?.result.school} `}
+          </Typography>
+          <Spacing direction="vertical" size={50} />
+          <Flex column="start" className="w-11/12 gap-2">
+            {edit && profile?.result.academyRole === ACADEMY_ROLE.STUDENT && (
+              <Flex row="between" className="w-full items-center gap-10">
+                <Typography size="h5" as="p" className="w-14">
+                  학교
                 </Typography>
                 {edit ? (
-                  <input
-                    defaultValue={profile?.result.introduction}
-                    disabled={profileLoading}
-                    id="nickname"
-                    {...register('introduction', { required: true })}
-                    placeholder="소개를 입력해주세요."
-                    className="w-full border-b border-solid border-b-gray-200 bg-transparent p-2 text-sm focus:border-b-main-deep-pink focus:outline-none lg:text-base"
-                  />
+                  <Input register={register} errors={errors} id="school" label="학교" variant="secondary" required />
                 ) : (
-                  <Typography size="h5" as="p" className="w-full p-2 text-sm font-normal opacity-80 lg:text-base">
+                  <Typography size="h5" as="p" className="font-normal">
                     {profile?.result.introduction}
                   </Typography>
                 )}
               </Flex>
-              {profile?.result.academyRole === ACADEMY_ROLE.STUDENT && (
-                <Flex className="items-center gap-2">
-                  <Typography size="h5" as="p" className="w-20 text-center text-sm font-semibold opacity-80 lg:text-base">
-                    학교
-                  </Typography>
-                  <Typography size="h5" as="p" className="w-full p-4 text-sm font-normal opacity-80 lg:text-base">
-                    {profile?.result.school}
-                  </Typography>
-                </Flex>
+            )}
+            <Flex row="between" className="w-full items-center gap-10">
+              <Typography size="h5" as="p" className="w-14">
+                닉네임
+              </Typography>
+              {edit ? (
+                <Input register={register} errors={errors} id="nickname" label="닉네임" variant="secondary" />
+              ) : (
+                <Typography size="h5" as="p" className="font-normal">
+                  {profile?.result.nickname}
+                </Typography>
               )}
-              {profile?.result.academyRole === ACADEMY_ROLE.STUDENT && (
-                <Flex className="items-center gap-2">
-                  <Typography size="h5" as="p" className="w-20 text-center text-sm font-semibold opacity-80 lg:text-base">
-                    포인트
-                  </Typography>
-                  <Typography size="h5" as="p" className="w-full p-4 text-sm font-normal opacity-80 lg:text-base">
-                    {profile?.result.point}
-                  </Typography>
-                </Flex>
+            </Flex>
+            <Flex row="between" className="w-full items-center gap-10">
+              <Typography size="h5" as="p" className="w-14">
+                소개
+              </Typography>
+              {edit ? (
+                <Input register={register} errors={errors} id="introduction" label="소개" variant="secondary" required />
+              ) : (
+                <Typography size="h5" as="p" className="font-normal">
+                  {profile?.result.introduction}
+                </Typography>
               )}
             </Flex>
           </Flex>
         </BoxContainer>
         <Flex row="end">
           {edit && (
-            <Button type="button" className="w-44 font-medium text-white" onClick={handleSubmit(onSubmit)}>
+            <Button type="button" className="w-fit px-2 py-1 text-sm font-medium text-white" onClick={handleSubmit(onSubmit)}>
               수정하기
             </Button>
           )}
         </Flex>
       </section>
-    </Flex>
+    </FramerScale>
   );
 }
 
