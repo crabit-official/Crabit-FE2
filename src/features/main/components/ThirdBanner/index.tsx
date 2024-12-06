3264e0b8d025da35423f7a0cb7d056a179b3a4a2
hijjoy <hijjoy@naver.com>
2024-12-07 01:27:24 +0900

    feat: main nsc 로고 수정

--- src/features/main/components/ThirdBanner/index.tsx
+++ src/features/main/components/ThirdBanner/index.tsx
@@ -11,22 +11,18 @@ function ThirdBanner() {
         alt="nsc logo"
         width={500}
         height={500}
-        className="absolute right-[-50px] top-0 hidden h-full w-fit object-fill sm:block"
+        className="absolute right-[-50px] top-0 hidden h-full w-fit object-fill md:block"
       />
+      <div className="absolute inset-0 bg-[url('https://cdn.imweb.me/thumbnail/20240910/78a516cad9749.png')] bg-cover bg-center bg-no-repeat md:hidden" />
       <Flex column="start" className="z-10 w-full gap-16">
         <Flex column="center" className="gap-2">
-          <Typography size="h5" className="text-sm font-medium text-blue-200 sm:text-base sm:font-bold">
+          <Typography size="h5" className="text-sm font-bold text-blue-200 sm:text-base">
             크래빗 X 한국NCS자격개발원
           </Typography>
           <Typography size="h2" className="whitespace-pre-wrap text-lg font-bold sm:text-2xl sm:font-extrabold">
             {`크래빗 추천 자격증 과정\nNCS자격개발원과 지금 시작해 보세요`}
           </Typography>
         </Flex>
-        {/* <div className="w-32 sm:w-36"> */}
-        {/*  <Button variant="outline" className="rounded-4xl bg-black text-xs font-medium text-white sm:text-base sm:font-bold"> */}
-        {/*    지금 수강하기 */}
-        {/*  </Button> */}
-        {/* </div> */}
       </Flex>
     </Flex>
   );
