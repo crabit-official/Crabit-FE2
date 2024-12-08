b73de77f419f44c8fc31b463931524ce08d6d713
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-12-09 00:21:28 +0900

    [Deploy] 2024년 12월 9일 Daily Deploy (#266)
    * [CRAB-108] 1차 QA 반영사항 수정 (#265)
    
    * feat: 챌린지 마켓 문구 수정
    
    * feat: 학생 인증글 날짜 추가
    - 날짜 추가
    - 스캘레톤 추가
    
    * feat: 초대코드 관리자 학교 필드 삭제
    
    ---------
    
    Co-authored-by: 김용민 <dydals3440@gmail.com>

--- src/app/academy/(workspace)/[id]/dashboard/components/StudentChallengeContents/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/StudentChallengeContents/index.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import React, { useEffect } from 'react';
+import { FaRegCalendarCheck } from 'react-icons/fa6';
 import { useInView } from 'react-intersection-observer';
 
 import StudentChallengeContent from '../StudentChallengeContent';
 
 import FallbackMessage from '@/shared/components/FallbackMessage';
 import Flex from '@/shared/components/Flex';
+import Skeleton from '@/shared/components/Skeleton/Skeleton';
+import Typography from '@/shared/components/Typography';
 import useGetInfiniteStudentChallengeContents from '@/shared/hooks/challenge/useGetInfiniteStudentChallengeContents';
 
 interface IStudentChallengeContents {
@@ -48,18 +51,26 @@ function StudentChallengeContents({ academyId, releasedChallengeId, studentChall
         <Flex rowColumn="center" className="z-10 gap-6 pt-10">
           {contents?.pages.map((page) =>
             page.result.challengeLogList.map((content) => (
-              <StudentChallengeContent
-                academyId={academyId}
-                key={content.challengeLog.studentChallengeLogId}
-                challengeLog={content.challengeLog}
-                studentProfile={content.studentProfile}
-              />
+              <Flex key={content.challengeLog.studentChallengeLogId} className="w-full flex-col sm:flex-row">
+                <Flex className="w-fit gap-1 py-2 opacity-60 sm:w-20">
+                  <FaRegCalendarCheck size={13} />
+                  <Typography size="h7" as="p" className="text-xs font-normal">
+                    {content.challengeLog.day}일차
+                  </Typography>
+                </Flex>
+                <StudentChallengeContent academyId={academyId} challengeLog={content.challengeLog} studentProfile={content.studentProfile} />
+              </Flex>
             )),
           )}
           {isFetching
             ? Array(6)
                 .fill('')
-                .map((_, i) => <StudentChallengeContent.Skeleton key={i} />)
+                .map((_, i) => (
+                  <Flex key={i} className="w-full flex-col gap-2 sm:flex-row">
+                    <Skeleton height={15} width={45} className="my-2 rounded-md sm:mx-3" />
+                    <StudentChallengeContent.Skeleton />
+                  </Flex>
+                ))
             : null}
           <div ref={ref} className="h-5" />
         </Flex>
