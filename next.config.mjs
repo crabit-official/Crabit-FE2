04836fd673bd7b87fec26917e1d70ff26f73cd23
김용민 <dydals3440@gmail.com>
2024-12-08 15:05:36 +0900

    [Deploy] 2024년 12월 8일 Daily Deploy (#260)
    * [CRAB-106] 개인정보 처리 방침 등록, s3 image-url 환경변수 변경, 첨부파일, UI/UX 수정 (#259)
    
    * feat: 개인정보 처리방침 추가
    
    * feat: Config 수정
    
    * feat: 첨부파일, UI/UX 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            if (Array.isArray(config.resolve.alias))
-                // server일시 browser를 제외 시킨다
-                config.resolve.alias.push({ name: "msw/browser", alias: false });
-            else config.resolve.alias["msw/browser"] = false;
-        } else if (Array.isArray(config.resolve.alias))
-                config.resolve.alias.push({ name: "msw/node", alias: false });
-            else config.resolve.alias["msw/node"] = false;
-        return config;
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      if (Array.isArray(config.resolve.alias))
+        // server일시 browser를 제외 시킨다
+        config.resolve.alias.push({ name: 'msw/browser', alias: false });
+      else config.resolve.alias['msw/browser'] = false;
+    } else if (Array.isArray(config.resolve.alias)) config.resolve.alias.push({ name: 'msw/node', alias: false });
+    else config.resolve.alias['msw/node'] = false;
+    return config;
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
     },
-    logging: {
-        fetches: {
-            fullUrl: true,
-        },
-    },
-    images: {
-        remotePatterns: [
-            // 통신 규약 패턴
-            {
-                protocol: "https",
-                hostname: "plus.unsplash.com",
-                port: "",
-                pathname: "/**",
-            },
-            {
-                protocol: "https",
-                hostname: 'crabit-bucket-dev.s3.ap-northeast-2.amazonaws.com',
-                port: "",
-                pathname: "/**",
-            },
-
-        ],
-    },
+  },
+  images: {
+    remotePatterns: [
+      // 통신 규약 패턴
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'crabit-bucket-dev.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'crabit-bucket-prod.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
