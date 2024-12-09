e8dc7f2848621eb163d23872d8dbeea503d6b46c
김용민 <dydals3440@gmail.com>
2024-12-10 02:41:38 +0900

    [Deploy] 2024년 12월 10일 Daily Deploy (#285)
    * fix: S3 환경, fileName 특수문자 관련 Sanitize 처리

--- src/app/api/challenge/list/route.ts
+++ src/app/api/challenge/list/route.ts
@@ -4,29 +4,25 @@ import { fetchData } from '@/shared/apis/fetch-data';
 import type { TGetMyAcademyChallengeListResponse } from '@/shared/types/acadmy';
 
 export async function GET(req: NextRequest) {
-    const cursor = req.nextUrl.searchParams.get('cursor') || '';
-    const take = req.nextUrl.searchParams.get('take') || '';
-    const academyId = req.nextUrl.searchParams.get('academyId') || '';
-    const challengeCategory = req.nextUrl.searchParams.get('challengeCategory') || '';
-    const title = req.nextUrl.searchParams.get('title') || '';
-    const challengeFilter = req.nextUrl.searchParams.get('challengeFilter') || '';
+  const cursor = req.nextUrl.searchParams.get('cursor') || '';
+  const take = req.nextUrl.searchParams.get('take') || '';
+  const academyId = req.nextUrl.searchParams.get('academyId') || '';
+  const challengeCategory = req.nextUrl.searchParams.get('challengeCategory') || '';
+  const title = req.nextUrl.searchParams.get('title') || '';
+  const challengeFilter = req.nextUrl.searchParams.get('challengeFilter') || '';
 
-    let url = `/api/v1/academies/${academyId}/challenges?challengeFilter=${challengeFilter}&cursor=${cursor}&take=${take}`;
+  let url = `/api/v1/academies/${academyId}/challenges?challengeFilter=${challengeFilter}&cursor=${cursor}&take=${take}`;
 
-    console.log(url);
+  if (challengeCategory) url += `&challengeCategory=${challengeCategory}`;
+  if (title) url += `&title=${title}`;
 
-    if (challengeCategory) url += `&challengeCategory=${challengeCategory}`;
-    if (title) url += `&title=${title}`;
+  try {
+    const data = await fetchData<TGetMyAcademyChallengeListResponse>(url, 'GET');
 
-    console.log(url);
+    return NextResponse.json(data);
+  } catch (e) {
+    const errorMessage = e instanceof Error ? e.message : '알수없는 에러가 발생했습니다.';
 
-    try {
-        const data = await fetchData<TGetMyAcademyChallengeListResponse>(url, 'GET');
-
-        return NextResponse.json(data);
-    } catch (e) {
-        const errorMessage = e instanceof Error ? e.message : '알수없는 에러가 발생했습니다.';
-
-        return NextResponse.json({ error: errorMessage }, { status: 400, statusText: 'Error Failed' });
-    }
+    return NextResponse.json({ error: errorMessage }, { status: 400, statusText: 'Error Failed' });
+  }
 }
--- src/shared/hooks/images/use-get-presigned-url.ts
+++ src/shared/hooks/images/use-get-presigned-url.ts
@@ -2,11 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 
 import type { TCreateS3PresignedUrlRequest, TCreateS3PresignedUrlResponse } from '@/shared/types/image';
 
+const sanitizeFileName = (fileName: string): string => fileName.replace(/[&$+\s;/:,?%#|`^{}[\]<>\\\r\n"`~|]/g, '_');
+
 const getPresignedUrl = async ({ fileName, s3Folder }: TCreateS3PresignedUrlRequest) => {
+  const sanitizedFileName = sanitizeFileName(fileName);
+
   const res = await fetch(`/api/image/upload`, {
     method: 'POST',
     body: JSON.stringify({
-      fileName,
+      fileName: sanitizedFileName,
       s3Folder,
     }),
   });
