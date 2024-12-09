fd27ba9403ffdbaaaceda16082976fb076124d03
김용민 <dydals3440@gmail.com>
2024-12-10 02:33:56 +0900

    [CRAB-116] S3 환경, fileName 특수문자 관련 Sanitize 처리. (#284)
    * fix: S3 환경, fileName 특수문자 관련 Sanitize 처리
    
    * fix: S3 환경, fileName 특수문자 관련 Sanitize 처리

--- src/app/academy/(workspace)/[id]/dashboard/components/(principal)/DashBoardUI/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/(principal)/DashBoardUI/index.tsx
@@ -7,27 +7,27 @@ import { getMyAcademyChallengeList } from '@/shared/apis/academy';
 import { queryKeys } from '@/shared/constants/query-keys';
 
 interface IDashboardProps {
-    academyId: number;
-    category: string;
-    challengeFilter: 'ALL' | 'CREATED_BY_ME' | 'RELEASED_FROM_MARKET';
-    search: string;
+  academyId: number;
+  category: string;
+  challengeFilter: 'ALL' | 'CREATED_BY_ME' | 'RELEASED_FROM_MARKET';
+  search: string;
 }
 
 async function PrincipalDashBoardUIl({ academyId, category, search, challengeFilter }: IDashboardProps) {
-    const queryClient = new QueryClient();
-    await queryClient.prefetchInfiniteQuery({
-        queryKey: [queryKeys.CHALLENGE_LIST],
-        queryFn: () => getMyAcademyChallengeList({ cursor: 0, take: 6, academyId, challengeFilter: 'ALL' }),
-        initialPageParam: 0,
-        getNextPageParam: (lastPage) => (lastPage.result.hasNext ? lastPage.result.nextCursor : undefined),
-        pages: 1,
-    });
-    const dehydratedState = dehydrate(queryClient);
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [queryKeys.CHALLENGE_LIST],
+    queryFn: () => getMyAcademyChallengeList({ cursor: 0, take: 6, academyId, challengeFilter: 'ALL' }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => (lastPage.result.hasNext ? lastPage.result.nextCursor : undefined),
+    pages: 1,
+  });
+  const dehydratedState = dehydrate(queryClient);
 
-    return (
-        <HydrationBoundary state={dehydratedState}>
-            <AllChallengeContents academyId={academyId} category={category} search={search} challengeFilter={challengeFilter} />
-        </HydrationBoundary>
-    );
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <AllChallengeContents academyId={academyId} category={category} search={search} challengeFilter={challengeFilter} />
+    </HydrationBoundary>
+  );
 }
 export default PrincipalDashBoardUIl;
--- src/shared/apis/academy.ts
+++ src/shared/apis/academy.ts
@@ -1,49 +1,49 @@
 import type { Session } from 'next-auth';
 
 import type {
-    TGetAcademyInfoRequest,
-    TGetAcademyInfoResponse,
-    TLeaveAcademyRequest,
-    TLeaveAcademyResponse,
-    TRevokeInstructorRequest,
-    TRevokeInstructorResponse,
-    TRevokeStudentResponse,
-    TUpdateAcademyInfoRequest,
-    TUpdateAcademyInfoResponse,
-    TUpdateInstructorIntroductionRequest,
-    TUpdateInstructorIntroductionResponse,
-    TUpdateStudentIntroductionRequest,
-    TUpdateStudentIntroductionResponse,
+  TGetAcademyInfoRequest,
+  TGetAcademyInfoResponse,
+  TLeaveAcademyRequest,
+  TLeaveAcademyResponse,
+  TRevokeInstructorRequest,
+  TRevokeInstructorResponse,
+  TRevokeStudentResponse,
+  TUpdateAcademyInfoRequest,
+  TUpdateAcademyInfoResponse,
+  TUpdateInstructorIntroductionRequest,
+  TUpdateInstructorIntroductionResponse,
+  TUpdateStudentIntroductionRequest,
+  TUpdateStudentIntroductionResponse,
 } from '../types/manage';
 
 import type { CommonResponse } from '@/shared/apis/dto/response';
 import type {
-    IAcademyProfile,
-    IAcademyResult,
-    IStatisticsResult,
-    ITop5StudentsResult,
-    TAcademyInstructorDetailRequest,
-    TAcademyInstructorDetailResponse,
-    TAcademyMemberEditProfileRequest,
-    TAcademyMemberProfileRequest,
-    TAcademyMemberProfileResponse,
-    TAcademyMemberRequest,
-    TAcademyMemberResponse,
-    TAcademyStudentDetailResponse,
-    TError,
-    TGetMyAcademyChallengeListRequest,
-    TGetMyAcademyChallengeListResponse,
+  IAcademyProfile,
+  IAcademyResult,
+  IStatisticsResult,
+  ITop5StudentsResult,
+  TAcademyInstructorDetailRequest,
+  TAcademyInstructorDetailResponse,
+  TAcademyMemberEditProfileRequest,
+  TAcademyMemberProfileRequest,
+  TAcademyMemberProfileResponse,
+  TAcademyMemberRequest,
+  TAcademyMemberResponse,
+  TAcademyStudentDetailResponse,
+  TError,
+  TGetMyAcademyChallengeListRequest,
+  TGetMyAcademyChallengeListResponse,
 } from '@/shared/types/acadmy';
 
 interface IGetAcademyList {
-    cursor: number;
-    take: number;
+  cursor: number;
+  take: number;
 }
 
 export async function getAcademyList({ cursor, take }: IGetAcademyList) {
-    const res = await fetch(`/api/academy/list?cursor=${cursor}&take=${take}`);
+  const res = await fetch(`/api/academy/list?cursor=${cursor}&take=${take}`);
 
-    return (await res.json()) as IAcademyResult;
+  return (await res.json()) as IAcademyResult;
 }
 
 // export async function postEnrollAcademy({
@@ -91,210 +91,210 @@ export async function getAcademyList({ cursor, take }: IGetAcademyList) {
 
 // TOP 5 학생 정보 조회
 export async function getTop5Students({ academyId, session }: { academyId: number; session: Session }) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${academyId}/statistics/monthly-point-ranking`, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
-        },
-    });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${academyId}/statistics/monthly-point-ranking`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
 
-    const data: ITop5StudentsResult = await res.json();
+  const data: ITop5StudentsResult = await res.json();
 
-    if (!data.isSuccess) {
-        throw new Error(data.message);
-    }
+  if (!data.isSuccess) {
+    throw new Error(data.message);
+  }
 
-    return data.result;
+  return data.result;
 }
 
 // 학원 챌린지 정보조회
 export async function getStatistics({ academyId, session }: { academyId: number; session: Session }) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${academyId}/statistics/challenges`, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${session?.accessToken}`,
-        },
-    });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/academies/${academyId}/statistics/challenges`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${session?.accessToken}`,
+    },
+  });
 
-    const data: IStatisticsResult = await res.json();
+  const data: IStatisticsResult = await res.json();
 
-    return data.result;
+  return data.result;
 }
 
 // 학원 프로필
 export async function getAcademyProfile(academyId: number) {
-    const res = await fetch(`/api/academy/profile?academyId=${academyId}`, {
-        method: 'GET',
-    });
+  const res = await fetch(`/api/academy/profile?academyId=${academyId}`, {
+    method: 'GET',
+  });
 
-    if (!res.ok) {
-        const errorData: TError = await res.json();
-        throw new Error(errorData.error);
-    }
+  if (!res.ok) {
+    const errorData: TError = await res.json();
+    throw new Error(errorData.error);
+  }
 
-    return (await res.json()) as CommonResponse<IAcademyProfile>;
+  return (await res.json()) as CommonResponse<IAcademyProfile>;
 }
 
 // 학원 정보 수정
 export async function updateAcademyInfo({ academyId, address, addressDetail, contactNumber, academyName, mainImageUrl }: TUpdateAcademyInfoRequest) {
-    const res = await fetch(`/api/manage/academy?academyId=${academyId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            address,
-            addressDetail,
-            contactNumber,
-            mainImageUrl,
-            academyName,
-        }),
-    });
+  const res = await fetch(`/api/manage/academy?academyId=${academyId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      address,
+      addressDetail,
+      contactNumber,
+      mainImageUrl,
+      academyName,
+    }),
+  });
 
-    return (await res.json()) as TUpdateAcademyInfoResponse;
+  return (await res.json()) as TUpdateAcademyInfoResponse;
 }
 
 // 학원 스페이스 탈퇴
 export async function leaveAcademy({ academyId }: TLeaveAcademyRequest) {
-    const res = await fetch(`/api/manage/academy?academyId=${academyId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  const res = await fetch(`/api/manage/academy?academyId=${academyId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    return (await res.json()) as TLeaveAcademyResponse;
+  return (await res.json()) as TLeaveAcademyResponse;
 }
 
 // 특정 학생 닉네임, 부가 설명 수정
 export async function updateStudentIntroduction({ academyId, academyMemberId, description, nickname }: TUpdateStudentIntroductionRequest) {
-    const res = await fetch(`/api/manage/student/edit?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ description, nickname }),
-    });
+  const res = await fetch(`/api/manage/student/edit?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ description, nickname }),
+  });
 
-    return (await res.json()) as TUpdateStudentIntroductionResponse;
+  return (await res.json()) as TUpdateStudentIntroductionResponse;
 }
 
 // 학원생 탈퇴
 export async function revokeStudent({ academyId, academyMemberId }: TRevokeInstructorRequest) {
-    const res = await fetch(`/api/manage/student/revoke?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  const res = await fetch(`/api/manage/student/revoke?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    return (await res.json()) as TRevokeStudentResponse;
+  return (await res.json()) as TRevokeStudentResponse;
 }
 
 // 특정 강사에 대한 부가 설명 수정
 export async function updateInstructorIntroduction({ academyId, academyMemberId, description }: TUpdateInstructorIntroductionRequest) {
-    const res = await fetch(`/api/manage/instructor/edit?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ description }),
-    });
+  const res = await fetch(`/api/manage/instructor/edit?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ description }),
+  });
 
-    return (await res.json()) as TUpdateInstructorIntroductionResponse;
+  return (await res.json()) as TUpdateInstructorIntroductionResponse;
 }
 
 // 강사 강퇴 기능
 export async function revokeInstructor({ academyId, academyMemberId }: TRevokeInstructorRequest) {
-    const res = await fetch(`/api/manage/instructor/revoke?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  const res = await fetch(`/api/manage/instructor/revoke?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    return (await res.json()) as TRevokeInstructorResponse;
+  return (await res.json()) as TRevokeInstructorResponse;
 }
 
 // 학원 정보 조회기능
 export async function getAcademyInfo({ academyId }: TGetAcademyInfoRequest) {
-    const res = await fetch(`/api/manage/academy?academyId=${academyId}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+  const res = await fetch(`/api/manage/academy?academyId=${academyId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    return (await res.json()) as TGetAcademyInfoResponse;
+  return (await res.json()) as TGetAcademyInfoResponse;
 }
 
 // 학생 관리 상세
 export async function getStudentDetail({ academyId, academyMemberId }: TRevokeInstructorRequest) {
-    const res = await fetch(`/api/manage/student?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
-        method: 'GET',
-    });
+  const res = await fetch(`/api/manage/student?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
+    method: 'GET',
+  });
 
-    return (await res.json()) as TAcademyStudentDetailResponse;
+  return (await res.json()) as TAcademyStudentDetailResponse;
 }
 
 // [학원 모든 멤버] 특정 학원 스페이스 유저 프로필 조회
 export async function getAcademyMemberProfile({ academyId }: TAcademyMemberProfileRequest) {
-    const res = await fetch(`/api/academy/profile?academyId=${academyId}`, {
-        method: 'GET',
-    });
+  const res = await fetch(`/api/academy/profile?academyId=${academyId}`, {
+    method: 'GET',
+  });
 
-    return (await res.json()) as TAcademyMemberProfileResponse;
+  return (await res.json()) as TAcademyMemberProfileResponse;
 }
 
 // [학원 모든 멤버] 특정 학원 스페이스 유저 프로필 수정
 export async function editAcademyMemberProfile({ academyId, nickname, introduction, school, profileImageUrl }: TAcademyMemberEditProfileRequest) {
-    const res = await fetch(`/api/academy/profile?academyId=${academyId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ nickname, introduction, school, profileImageUrl }),
-    });
+  const res = await fetch(`/api/academy/profile?academyId=${academyId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ nickname, introduction, school, profileImageUrl }),
+  });
 
-    return (await res.json()) as TAcademyMemberProfileResponse;
+  return (await res.json()) as TAcademyMemberProfileResponse;
 }
 
 // [원장 선생님] 학원 특정 강사 상세 정보 조회
 export async function getAcademyInstructorDetail({ academyId, academyMemberId }: TAcademyInstructorDetailRequest) {
-    const res = await fetch(`/api/academy/profile/instructors?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
-        method: 'GET',
-    });
+  const res = await fetch(`/api/academy/profile/instructors?academyId=${academyId}&academyMemberId=${academyMemberId}`, {
+    method: 'GET',
+  });
 
-    return (await res.json()) as TAcademyInstructorDetailResponse;
+  return (await res.json()) as TAcademyInstructorDetailResponse;
 }
 
 // [원장 선생님] 학원 멤버 수 조회
 export async function getAcademyMemberCount({ academyId, academyRole }: TAcademyMemberRequest) {
-    const res = await fetch(`/api/academy/member/count?academyId=${academyId}&academyRole=${academyRole}`);
+  const res = await fetch(`/api/academy/member/count?academyId=${academyId}&academyRole=${academyRole}`);
 
-    return (await res.json()) as TAcademyMemberResponse;
+  return (await res.json()) as TAcademyMemberResponse;
 }
 
 // 우리 학원 배포 챌린지 리스트 조회
 export async function getMyAcademyChallengeList({
-                                                    academyId,
-                                                    challengeFilter,
-                                                    challengeCategory,
-                                                    title,
-                                                    cursor,
-                                                    take,
-                                                }: TGetMyAcademyChallengeListRequest): Promise<TGetMyAcademyChallengeListResponse> {
-    let url = `/api/challenge/list?cursor=${cursor}&take=${take}&academyId=${academyId}&challengeFilter=${challengeFilter}`;
+  academyId,
+  challengeFilter,
+  challengeCategory,
+  title,
+  cursor,
+  take,
+}: TGetMyAcademyChallengeListRequest): Promise<TGetMyAcademyChallengeListResponse> {
+  let url = `/api/challenge/list?cursor=${cursor}&take=${take}&academyId=${academyId}&challengeFilter=${challengeFilter}`;
 
-    if (challengeCategory) {
-        url += `&challengeCategory=${challengeCategory}`;
-    }
+  if (challengeCategory) {
+    url += `&challengeCategory=${challengeCategory}`;
+  }
 
-    if (title) {
-        url += `&title=${title}`;
-    }
+  if (title) {
+    url += `&title=${title}`;
+  }
 
-    const res = await fetch(url);
+  const res = await fetch(url);
 
-    return (await res.json()) as TGetMyAcademyChallengeListResponse;
+  return (await res.json()) as TGetMyAcademyChallengeListResponse;
 }
