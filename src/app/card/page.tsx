f32db910b4105417888ab6e0dfb0375f978db424
hijjoy <hijjoy@naver.com>
2024-12-16 15:23:05 +0900

    feat: 레이아웃 지정

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -2,13 +2,14 @@
 
 import CardApply from '@/features/card/components/CardApply';
 import CardInfo from '@/features/card/components/CardInfo';
+import Flex from '@/shared/components/Flex';
 
 function CrabitCardPage() {
   return (
-    <>
+    <Flex column="start" className="gap-10">
       <CardInfo />
       <CardApply />
-    </>
+    </Flex>
   );
 }
 
