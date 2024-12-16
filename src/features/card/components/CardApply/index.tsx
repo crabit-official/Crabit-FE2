e085ea74dc66683fe803162d216ac8782de06332
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-12-16 15:40:54 +0900

    [CRAB-126] 장학카드 신청하기  (#310)
    * feat: 카드 신청하기 추가
    
    * feat: 가격 안내 추가
    
    * feat: 레이아웃 지정
    
    * feat: 공개 챌린지 확인 모달 변경
    
    * feat: 로그인 후 내 기관 이동
    
    * feat: 등록하기 레이아웃 수정

---
+++ src/features/card/components/CardApply/index.tsx
@@ -0,0 +1,57 @@
+import { FaArrowDownLong } from 'react-icons/fa6';
+import { IoIosArrowForward } from 'react-icons/io';
+import Link from 'next/link';
+
+import StateLabel from '@/features/academy/(workspace)/components/state-label';
+import Flex from '@/shared/components/Flex';
+import Framer from '@/shared/components/Framer';
+import Typography from '@/shared/components/Typography';
+import { CARD_APPLY1, CARD_APPLY2 } from '@/shared/constants/card';
+
+function CardApply() {
+  return (
+    <Framer className="flex w-full flex-col items-center justify-between gap-10 px-10 md:justify-between md:px-20 xl:px-40">
+      <Flex column="start" className="w-full gap-2">
+        <StateLabel label="카드 신청" className="w-fit" />
+        <Typography size="h2">신청 & 주문 과정</Typography>
+        <Flex rowColumn="center" className="mt-5 w-full gap-4">
+          <ul className="flex h-52 w-full flex-col justify-center gap-3 rounded-2xl border border-solid border-gray-100 bg-[#fafafa] p-5">
+            {CARD_APPLY1.map((item, idx) => (
+              <li key={idx}>{item.content}</li>
+            ))}
+          </ul>
+          <Flex rowColumn="center" className="gap-3">
+            <Flex rowColumn="center" className="gap-1">
+              <Typography size="h5">❼ 발송</Typography>
+              <Typography size="h6" className="opacity-80">
+                디자인 컨펌 기준 평균 3일 이내로 제작 후 발송
+              </Typography>
+            </Flex>
+            <FaArrowDownLong />
+          </Flex>
+          <ul className="flex h-52 w-full flex-col justify-center gap-5 rounded-2xl border border-solid border-gray-100 bg-[#fafafa] p-5">
+            {CARD_APPLY2.map((item, idx) => (
+              <li key={idx} className="flex flex-col gap-1">
+                <p>{item.content}</p>
+                {item?.sub && <p className="text-sm opacity-80">({item.sub})</p>}
+              </li>
+            ))}
+          </ul>
+        </Flex>
+      </Flex>
+      <Link href="/pricing" className="flex w-full items-center justify-end gap-2 px-2 opacity-80 hover:text-main-deep-pink">
+        <p>가격 안내</p>
+        <IoIosArrowForward />
+      </Link>
+      <Link
+        href="https://smore.im/form/XRusYYBvZ8"
+        target="_blank"
+        className="rounded-4xl bg-main-deep-pink px-10 py-3 text-white transition duration-300 hover:scale-95 hover:opacity-80"
+      >
+        카드 신청하기
+      </Link>
+    </Framer>
+  );
+}
+
+export default CardApply;
