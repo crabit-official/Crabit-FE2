6c4a4c14e132888cea6a60054ec000abf00571b0
hijjoy <hijjoy@naver.com>
2024-12-27 12:40:00 +0900

    feat: 챌린지 인증 UI 변경

--- src/app/academy/(workspace)/[id]/dashboard/components/(student)/CreateChallengeForm/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/(student)/CreateChallengeForm/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { type FieldValues, useForm } from 'react-hook-form';
+import { FaFile } from 'react-icons/fa';
 import { IoMdPhotos } from 'react-icons/io';
 import Image from 'next/image';
+import Link from 'next/link';
 import { toast } from 'sonner';
 
+import Toggle from '@/app/academy/(workspace)/[id]/dashboard/components/Toggle';
 import { useImage } from '@/features/academy/(workspace)/hooks/use-image';
+import { getChallengeCategory, getChallengeType } from '@/features/academy/(workspace)/utils/challengeState';
 import Button from '@/shared/components/Button';
 import Flex from '@/shared/components/Flex';
 import TextArea from '@/shared/components/Textarea';
@@ -13,6 +17,7 @@ import useCreateChallengeContent from '@/shared/hooks/challenge/useCreateChallen
 import useGetPresignedUrl from '@/shared/hooks/images/use-get-presigned-url';
 import type { TMyChallengeProgressResult } from '@/shared/types/acadmy';
 import { S3_FOLDER_NAME } from '@/shared/types/image';
+import { formatNumberWithCommas } from '@/shared/utils/number';
 
 interface ICreateChallengeFormProps {
   academyId: number;
@@ -73,11 +78,66 @@ function CreateChallengeForm({ challengeData, academyId, studentChallengeId }: I
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 rounded-xl border border-solid border-gray-100 p-5 shadow-custom">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 rounded-xl border border-solid border-gray-100 p-5 pb-8 shadow-custom">
       <Flex column="start" className="gap-1">
-        <Typography size="h3" className="opacity-80">
-          챌린지 인증글 올리기
-        </Typography>
+        <Toggle
+          buttonOpenLabel="설명 확인"
+          buttonCloseLabel="설명 닫기"
+          title={`'${challengeData.releasedChallenge.title}'  인증글 작성`}
+          content={
+            <Flex column="center" className="gap-4">
+              <Typography size="h5" className="break-keep text-main-deep-pink">
+                {getChallengeType(challengeData.releasedChallenge.challengeType)} • {getChallengeCategory(challengeData.releasedChallenge.challengeCategory)}
+              </Typography>
+              {challengeData.releasedChallenge.thumbnailImageUrl ? (
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${challengeData.releasedChallenge.thumbnailImageUrl}`}
+                  alt="thumbnail image"
+                  width={500}
+                  height={500}
+                  className="h-64 w-full rounded-2xl border border-solid border-gray-100 object-contain shadow-custom"
+                />
+              ) : null}
+              <Flex column="center" className="gap-2">
+                <Typography size="h6" className="break-keep font-medium text-main-deep-pink" as="p">
+                  DAY {challengeData.releasedChallenge.totalDays} • Ⓟ {formatNumberWithCommas(challengeData.releasedChallenge.points)}
+                </Typography>
+                <Typography size="h5" className="overflow-hidden whitespace-pre-wrap break-all font-normal opacity-80" as="p">
+                  {challengeData.releasedChallenge.content}
+                </Typography>
+                {challengeData?.releasedChallenge.description && (
+                  <>
+                    <Typography size="h6" as="p" className="mt-">
+                      챌린지 추가 설명
+                    </Typography>
+                    <Typography size="h5" className="overflow-hidden whitespace-pre-wrap break-all font-normal opacity-80" as="p">
+                      {challengeData.releasedChallenge.description}
+                    </Typography>
+                  </>
+                )}
+              </Flex>
+              {/* TODO: 챌린지 올린 사람이, 올린 파일을 볼 수 있는 자리. */}
+              {challengeData.releasedChallenge.fileUrl && (
+                <Flex column="start" className="w-full gap-2 rounded-xl bg-neutral-50 p-5">
+                  <Typography size="h5" className="break-keep font-bold opacity-80" as="p">
+                    첨부 파일
+                  </Typography>
+                  <Flex row="start">
+                    <FaFile className="mr-2" />
+                    <Link
+                      target="_blank"
+                      href={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${challengeData.releasedChallenge.fileUrl}`}
+                      download
+                      className="text-blue-500 underline"
+                    >
+                      {challengeData.releasedChallenge.fileUrl.split('_').slice(1).join('_')}
+                    </Link>
+                  </Flex>
+                </Flex>
+              )}
+            </Flex>
+          }
+        />
         <Flex as="figure" column="center" className="mt-2 gap-2">
           <label
             htmlFor="file"
--- src/app/academy/(workspace)/[id]/dashboard/components/(student)/MyChallengeDetail/index.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/(student)/MyChallengeDetail/index.tsx
@@ -1,18 +1,11 @@
 'use client';
 
 import React from 'react';
-import { FaFile } from 'react-icons/fa';
-import Image from 'next/image';
-import Link from 'next/link';
 
 import ChallengeStatusBar from '@/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeStatusBar/ChallengeStatusBar';
 import CreateChallengeForm from '@/app/academy/(workspace)/[id]/dashboard/components/(student)/CreateChallengeForm';
-import Toggle from '@/app/academy/(workspace)/[id]/dashboard/components/Toggle';
-import { getChallengeCategory, getChallengeType } from '@/features/academy/(workspace)/utils/challengeState';
 import Flex from '@/shared/components/Flex';
-import Typography from '@/shared/components/Typography';
 import type { TMyChallengeProgressResult } from '@/shared/types/acadmy';
-import { formatNumberWithCommas } from '@/shared/utils/number';
 
 interface IMyChallengeDetailProps {
   academyId: number;
@@ -21,8 +14,6 @@ interface IMyChallengeDetailProps {
 }
 
 function MyChallengeDetail({ challengeData, studentChallengeId, academyId }: IMyChallengeDetailProps) {
-  console.log(challengeData);
-
   return (
     <Flex rowColumn="center" className="w-full gap-10 px-4">
       <Flex column="start" className="w-full gap-3">
@@ -32,67 +23,12 @@ function MyChallengeDetail({ challengeData, studentChallengeId, academyId }: IMy
           hasTodayChallengeLog={challengeData.studentChallenge.hasTodayChallengeLog}
           startedAt={challengeData.studentChallenge.startedAt}
         />
-        <Toggle
-          title={challengeData.releasedChallenge.title}
-          content={
-            <Flex column="center" className="gap-4">
-              <Typography size="h5" className="break-keep text-main-deep-pink">
-                {getChallengeType(challengeData.releasedChallenge.challengeType)} • {getChallengeCategory(challengeData.releasedChallenge.challengeCategory)}
-              </Typography>
-              {challengeData.releasedChallenge.thumbnailImageUrl ? (
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${challengeData.releasedChallenge.thumbnailImageUrl}`}
-                  alt="thumbnail image"
-                  width={500}
-                  height={500}
-                  className="h-64 w-full rounded-2xl border border-solid border-gray-100 object-contain shadow-custom"
-                />
-              ) : null}
-              <Flex column="center" className="gap-2">
-                <Typography size="h6" className="break-keep font-medium text-main-deep-pink" as="p">
-                  DAY {challengeData.releasedChallenge.totalDays} • Ⓟ {formatNumberWithCommas(challengeData.releasedChallenge.points)}
-                </Typography>
-                <Typography size="h5" className="overflow-hidden whitespace-pre-wrap break-all font-normal opacity-80" as="p">
-                  {challengeData.releasedChallenge.content}
-                </Typography>
-                {challengeData?.releasedChallenge.description && (
-                  <>
-                    <Typography size="h6" as="p" className="mt-5">
-                      챌린지 추가 설명
-                    </Typography>
-                    <Typography size="h5" className="overflow-hidden whitespace-pre-wrap break-all font-normal opacity-80" as="p">
-                      {challengeData.releasedChallenge.description}
-                    </Typography>
-                  </>
-                )}
-              </Flex>
-            </Flex>
-          }
-        />
       </Flex>
       <div className="grid w-full grid-cols-1 gap-4">
-        {/* TODO: 챌린지 올린 사람이, 올린 파일을 볼 수 있는 자리. */}
-        {challengeData.releasedChallenge.fileUrl && (
-          <>
-            <Typography size="h6" className="break-keep font-bold opacity-80" as="p">
-              첨부 파일
-            </Typography>
-            <Flex row="start">
-              <FaFile className="mr-2" />
-              <Link
-                target="_blank"
-                href={`${process.env.NEXT_PUBLIC_S3_IMAGES}/${challengeData.releasedChallenge.fileUrl}`}
-                download
-                className="text-blue-500 underline"
-              >
-                {challengeData.releasedChallenge.fileUrl.split('_').slice(1).join('_')}
-              </Link>
-            </Flex>
-          </>
-        )}
         {/* <Flex column="start" className="gap-4 rounded-xl border border-solid border-gray-100 bg-neutral-50 p-5 shadow-custom">
           파일뷰어 자리입니당... <br /> 아직 미완성 ..
         </Flex> */}
+
         <Flex column="center" className="w-full gap-4">
           <CreateChallengeForm academyId={academyId} studentChallengeId={studentChallengeId} challengeData={challengeData} />
         </Flex>
