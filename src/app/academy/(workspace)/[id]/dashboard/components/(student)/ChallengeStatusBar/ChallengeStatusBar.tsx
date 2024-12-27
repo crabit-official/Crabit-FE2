c78bc9fca36b10187dbe68957a4714dc4851a3f8
hijjoy <hijjoy@naver.com>
2024-12-27 13:42:32 +0900

    feat: 챌린지 인증 바 레이아웃 변경

--- src/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeStatusBar/ChallengeStatusBar.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeStatusBar/ChallengeStatusBar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import StateLabel from '@/features/academy/(workspace)/components/state-label';
 import { getChallengeLogStatus } from '@/features/academy/(workspace)/utils/challengeState';
-import Flex from '@/shared/components/Flex';
 import { CHALLENGE_LOG_SUBMISSION_STATUS } from '@/shared/enums/challenge';
 import calculateChallengeDay from '@/shared/utils/calculateChallengeDay';
 
@@ -17,12 +16,11 @@ export default function ChallengeStatusBar({ totalDays, challengeLogSubmissionSt
   const { variant, label } = getChallengeLogStatus(hasTodayChallengeLog, challengeLogSubmissionStatus);
 
   return (
-    <Flex
-      row="between"
-      className={`w-full items-center justify-start gap-4 rounded-lg border border-solid px-3 py-2 font-medium sm:gap-6 md:justify-between ${hasTodayChallengeLog || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.ALL_LOGS_SUBMITTED || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.SUBMISSION_FAILED ? 'border-none bg-gray-100 text-gray-500' : 'border-main-deep-pink/60 text-main-deep-pink'} `}
+    <div
+      className={`grid w-full grid-cols-[min-content_auto_min-content] items-center gap-4 rounded-lg border border-solid px-3 py-2 font-medium sm:gap-6 ${hasTodayChallengeLog || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.ALL_LOGS_SUBMITTED || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.SUBMISSION_FAILED ? 'border-none bg-gray-100 text-gray-500' : 'border-main-deep-pink/60 text-main-deep-pink'} `}
     >
       <StateLabel label={label} variant={variant} className="w-fit whitespace-nowrap" />
-      <p className="break-keep text-sm md:text-base">
+      <p className="break-keep text-center text-sm md:text-base">
         {hasTodayChallengeLog &&
           challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.IN_PROGRESS &&
           `이미 DAY ${calculateChallengeDay(startedAt)} 인증을 해주셨네요 ! 앞으로 ${totalDays - calculateChallengeDay(startedAt)}일만 더 힘내볼까요?`}
@@ -35,7 +33,7 @@ export default function ChallengeStatusBar({ totalDays, challengeLogSubmissionSt
         {challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.ALL_LOGS_SUBMITTED && '챌린지를 완료했어요 !'}
         {challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.SUBMISSION_FAILED && '챌린지 인증에 실패했어요'}
       </p>
-      <div className="hidden w-[60px] md:block" />
-    </Flex>
+      <div className="hidden w-[60px] sm:block" />
+    </div>
   );
 }
