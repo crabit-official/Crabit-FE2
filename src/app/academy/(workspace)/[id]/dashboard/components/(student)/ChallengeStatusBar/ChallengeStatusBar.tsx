46ae2d925538c1d635f5d6c28407af3d60167573
hijjoy <hijjoy@naver.com>
2024-12-27 13:34:17 +0900

    feat: 인증글 작성 모바일 화면 수정

--- src/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeStatusBar/ChallengeStatusBar.tsx
+++ src/app/academy/(workspace)/[id]/dashboard/components/(student)/ChallengeStatusBar/ChallengeStatusBar.tsx
@@ -19,23 +19,23 @@ export default function ChallengeStatusBar({ totalDays, challengeLogSubmissionSt
   return (
     <Flex
       row="between"
-      className={`w-full items-center gap-2 rounded-lg border border-solid px-3 py-2 font-medium ${hasTodayChallengeLog || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.ALL_LOGS_SUBMITTED || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.SUBMISSION_FAILED ? 'border-none bg-gray-100 text-gray-500' : 'border-main-deep-pink/60 text-main-deep-pink'} `}
+      className={`w-full items-center justify-start gap-4 rounded-lg border border-solid px-3 py-2 font-medium sm:gap-6 md:justify-between ${hasTodayChallengeLog || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.ALL_LOGS_SUBMITTED || challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.SUBMISSION_FAILED ? 'border-none bg-gray-100 text-gray-500' : 'border-main-deep-pink/60 text-main-deep-pink'} `}
     >
-      <StateLabel label={label} variant={variant} className="w-fit" />
-      <p>
+      <StateLabel label={label} variant={variant} className="w-fit whitespace-nowrap" />
+      <p className="break-keep text-sm md:text-base">
         {hasTodayChallengeLog &&
           challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.IN_PROGRESS &&
-          `이미 ${calculateChallengeDay(startedAt)}일차 인증을 해주셨네요! 앞으로 ${totalDays - calculateChallengeDay(startedAt)}일만 더 힘내볼까요?`}
+          `이미 DAY ${calculateChallengeDay(startedAt)} 인증을 해주셨네요 ! 앞으로 ${totalDays - calculateChallengeDay(startedAt)}일만 더 힘내볼까요?`}
 
         {!hasTodayChallengeLog &&
           challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.IN_PROGRESS &&
-          `챌린지의 Day ${calculateChallengeDay(startedAt)} 인증을 해주세요!`}
+          `챌린지의 Day ${calculateChallengeDay(startedAt)} 인증을 해주세요 !`}
 
         {challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.NOT_STARTED && '아직 챌린지를 시작하지 않았어요 ! 1일차 인증을 진행해주세요'}
         {challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.ALL_LOGS_SUBMITTED && '챌린지를 완료했어요 !'}
         {challengeLogSubmissionStatus === CHALLENGE_LOG_SUBMISSION_STATUS.SUBMISSION_FAILED && '챌린지 인증에 실패했어요'}
       </p>
-      <div className="w-[60px]" />
+      <div className="hidden w-[60px] md:block" />
     </Flex>
   );
 }
